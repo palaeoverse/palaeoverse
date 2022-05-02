233d851fa662f85317d4f1b15d5db799bd724aa1
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-05-02 11:08:50 +0200

    time bins and lat bins function
    Developed the functions for generating time bins and latitudinal bins

--- R/lat_bins.R
+++ R/lat_bins.R
@@ -2,16 +2,17 @@
 #'
 #' A function to generate latitudinal bins of a given size.
 #'
-#' @param size A single numeric value of more than 0, and less than or equal to 90.
-#' @param fit A logical value indicating whether a bin size check should be performed to ensure that the
-#' entire latitudinal range is covered (90ºS to 90ºN). If \code{fit = TRUE}, bin size is set to the nearest integer
+#' @param size \code{numeric}. A single numeric value of more than 0, and less than or equal to 90.
+#' @param fit \code{logical}. Should the bin size be checked to ensure that the entire
+#' latitudinal range is covered (90ºS to 90ºN)? If \code{fit = TRUE}, bin size is set to the nearest integer
 #' which is divisible into 180 (the entire latitudinal range).
+#' @param plot \code{logical}. Should a plot of the latitudinal bins be generated?
 #' @return A \code{data.frame} of latitudinal bins for a given size.
 #' @examples
 #' lat_bins(size = 20)
 #' lat_bins(size = 13, fit = TRUE)
 
-lat_bins <- function(size = 10, fit = FALSE){
+lat_bins <- function(size = 10, fit = FALSE, plot = TRUE){
   #divide latitudinal range by size of bins
   bins <- 180/size
   #if fit is set true, generate equal size bins to fit range
@@ -31,8 +32,23 @@ lat_bins <- function(size = 10, fit = FALSE){
   df <- cbind(max, mid, min)
   df <- df[order(-max),]
   df <- cbind.data.frame(bin, df)
-  print(df)
+
+  if(plot == TRUE){
+    plot(1, type = "n", xlim = c(-180, 180), ylim = c(max(df$max), min(df$min)), xlab = "Longitude (\u00B0)", ylab = "Latitude (\u00B0)")
+    cols <- rep(c("#3288bd", "#1a9850"), nrow(df))
+    for(i in 1:nrow(df)){
+      polygon(x = c(-180, -180, 180, 180),
+              y = c(df$min[i], df$max[i], df$max[i], df$min[i]),
+              col = cols[i],
+              border = "black")
+    }
+    if(fit == TRUE){
+      title(paste0("Bin size set to ", size))
+    }
+  }
+
   if(fit == TRUE){
     message(paste0("Bin size set to ",size," degrees to fit latitudinal range."))
   }
+  return(df)
 }
---
+++ R/time_bins.R
@@ -0,0 +1,125 @@
+#' Generate time bins
+#'
+#' A function to generate time bins for a given study interval.
+#'
+#' @param interval \code{character or numeric}. Interval name of age available in \code{GTS2020}. If a single interval name is provided, this interval is returned.
+#' If two interval names are provided, these intervals and those existing between are returned. If a single interval age is provided, the age matching this interval is returned.
+#' If two interval ages are provided, the intervals occurring in the range of these ages are returned.
+#' @param equal \code{logical}. Should equal-length time bins be generated?
+#' @param size \code{numeric}. If equal == \code{true}, specify the length in millions of years (Myr) of time bin desired. Defaults to 10 Myr.
+#' @param plot \code{logical}. Should a plot of time bins be generated?
+#' @return A \code{data.frame} of time bins for a specified interval.
+#' @examples
+#' Using interval midpoint age
+#' time_bins(interval = 10, equal = FALSE, plot = TRUE)
+#' Using interval age range
+#' time_bins(interval = c(50, 100), equal = FALSE, plot = TRUE)
+#' Using a single interval name
+#' time_bins(interval = c("Maastrichtian"), equal = FALSE, plot = TRUE)
+#' Using a range of intervals defined by two named intervals and equal duration bins
+#' time_bins(interval = c("Fortunian", "Meghalayan"), equal = TRUE, size = 10, plot = TRUE)
+time_bins <- function(interval = c("Fortunian", "Meghalayan"), equal = FALSE, size = 10, plot = TRUE){
+  #download data
+  df <- palaeoverse::GTS2020
+  #subset to stages
+  df <- subset(df, rank == "Stage")
+
+  #character string entered
+  if(is.character(interval)){
+    if(length(interval) == 1){
+      w <- which(df$interval_name %in% interval)
+      if(length(w) != length(interval)){stop(paste0("Checking spelling of specified intervals. \n Available intervals are: ",
+                                                    toString(df$interval_name)))}
+      df <- df[w,]
+    }
+    if(length(interval) == 2){
+      w <- which(df$interval_name %in% interval)
+      df <- df[w[1]:w[2],]
+    }
+  }
+  #numeric ages entered
+  if(is.numeric(interval)){
+    if(length(interval) == 1){
+      if(interval > max(df$max_ma) | interval < min(df$min_ma)){
+        stop("Value does not appear in range of available intervals: 0 to 541")
+      }
+      df <- df[which.min(abs(df$mid_ma - interval)),]
+    }
+
+    if(length(interval) == 2){
+      max_int <- max(interval)
+      min_int <- min(interval)
+
+      if(max_int > max(df$max_ma) | min_int < min(df$min_ma)){
+        stop("Values do not appear in range of available intervals: 0 to 541")
+      }
+
+      df <- df[which.min(abs(df$min_ma - min_int)):which.min(abs(df$max_ma - max_int)),]
+    }
+  }
+
+  if(length(interval) > 2){
+    stop("interval must be a character vector or a numeric vector of length 1 or 2")
+  }
+
+  #are equal length time bins required?
+  if(equal == TRUE){
+    #track cumulative sum
+    tracker <- list()
+    for(i in 1:nrow(df)){
+      tracker[[i]] <- rep(NA, length.out = nrow(df))
+      tracker[[i]][i:nrow(df)] <- abs(cumsum(df$duration_myr[i:nrow(df)]) - size)
+    }
+
+    #calculate upper and lower limits for each bin
+    lower <- NULL
+    upper <- NULL
+    count <- 1
+    while(count <= nrow(df)){
+      upper <- append(upper, which.min(tracker[[count]]))
+      lower <- append(lower, (count))
+      count <- which.min(tracker[[count]]) + 1
+    }
+
+    #generate bin information
+    bin <- 1:length(upper)
+    max_ma <- df[upper,c("max_ma")]
+    min_ma <- df[lower,c("min_ma")]
+    mid_ma <- (max_ma + min_ma)/2
+    duration_myr <- max_ma - min_ma
+    intervals <- vector("character")
+    #get interval names
+    for(i in 1:length(upper)){
+      intervals[i] <- toString(df[lower[i]:upper[i], c("interval_name")])
+    }
+    #generate dataframe
+    df <- cbind.data.frame(bin, max_ma, mid_ma, min_ma, duration_myr, intervals)
+    #message user
+    message(paste0("Target equal length time bins was set to ", size,
+                   " Myr. \nGenerated time bins have a mean length of ", round(mean(df$duration_myr), digits = 2),
+                   " Myr and a standard deviation of ", round(sd(df$duration_myr), digits = 2), " Myr."))
+  }
+
+  if(plot == TRUE){
+    if(equal == TRUE){df$colour <- c("#4292c6")}
+    plot(1, type = "n", xlim = c(max(df$max_ma), min(df$min_ma)), ylim = c(0, max(df$duration_myr)), xlab = "Time (Ma)", ylab = "Duration (Myr)")
+      for(i in 1:nrow(df)){
+        polygon(x = c(df$min_ma[i], df$max_ma[i], df$max_ma[i], df$min_ma[i]),
+                y = c(0, 0, df$duration_myr[i], df$duration_myr[i]),
+                col = df$colour[i])
+      }
+    if(equal == TRUE){
+      title(paste0("Mean bin length = ", round(mean(df$duration_myr), digits = 2),
+                   " (standard deviation = ", round(sd(df$duration_myr), digits = 2), ")"))
+      df <- df[,-which(colnames(df) == "colour")]
+    }
+
+    if(equal == FALSE){
+      df <- df[,c("interval_name", "max_ma", "mid_ma", "min_ma", "duration_myr")]
+    }
+
+    }
+
+  return(df)
+}
+
