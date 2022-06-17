20cba9c8035b8675f8dc2c5612c5d9b18ab6ca4b
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-06-17 18:39:38 +0200

    remove palaeorotation grids
    Removed palaeorotation grids

--- R/palaeorotate.R
+++ R/palaeorotate.R
@@ -1,36 +1,44 @@
 #' Palaeorotate fossil occurrences
 #'
 #' A function to generate stage-level palaeocoordinates (0--540 Ma) for fossil occurrence data (i.e. reconstruct the geographic distribution of organism's remains
-#' at time of deposition). Each occurrence are assigned palaeocoordinates based on its current geographic distribution and age estimate.
+#' at time of deposition). Each occurrence is assigned palaeocoordinates based on its current geographic distribution and age estimate.
 #'
 #' @param x \code{dataframe}. Fossil occurrences to be palaeogeographically reconstructed. \code{x} should be a dataframe containing the
 #' following named columns: "lng", "lat", "age". Age should be supplied in millions of years before present (Ma). This format is intentionally strict to ensure
 #' that data is entered correctly to prevent errors such as longitude and latitude being confused.
-#' @param model \code{character}. The name of the plate rotation model to use to reconstruct palaeocoordinates. Choose from: "Merdith2021", "Scotese2018", and "Wright2013".
+#' @param model \code{character}. The name of the plate rotation model to be used to reconstruct palaeocoordinates. Choose from: "Merdith2021", "Scotese2018", and "Wright2013".
 #' The default is "Merdith2021". See details below for further information on each model.
 #' @param uncertainty \code{logical}. Should uncertainty in palaeogeographic reconstructions be returned? If set to TRUE, the palaeocoordinates from the three plate rotation models
 #' are returned ("Merdith2021", "Scotese2018", and "Wright2013"), along with their respective longitudinal and latitudinal range.
 #'
 #' @return A \code{dataframe} containing the original input occurrence dataframe, age of rotation (Ma), and
 #' the reference coordinates rotated. "rot_age" refers to the age of rotation and is deduced from the reference age
-#' provided and closest midpoint age of a stratigraphic stage. "rot_lng" and "rot_lat" refer to the reference coordinates rotated.
+#' provided and the closest midpoint age of a stratigraphic stage. "rot_lng" and "rot_lat" refer to the reference coordinates rotated.
 #' "p_lng" and "p_lat" are the reconstructed coordinates.
 #'
 #' @details This function generates palaeocoordinates by spatiotemporally linking present-day geographic coordinates and age estimates with a spatial
 #' grid (1º x 1º) rotated to the midpoint of stratigraphic stages (GTS, 2020; \url{https://stratigraphy.org/timescale/}). As such, palaeocoordinates can be efficiently generated for large datasets with
-#' relatively little computational power. A further benefit of this approach is that no internet connection is required as in previously implemented API approaches.
-#' In addition, no additional software or knowledge is required from the user (i.e. GPlates). However, it should be noted that if specific ages of rotation are required,
+#' relatively little computational power. A further benefit of this approach is that no internet connection is required as in previously implemented API approaches
+#' (after initial installation of palaeoverseData). In addition, no additional software or knowledge is required from the user (i.e. GPlates). However, it should be noted that if specific ages of rotation are required,
 #' or fine-scale spatial analyses is being conducted, use of \url{https://www.gplates.org} might be preferable for the user.
 #'
-#' The current palaeorotations (0--540 Ma) provided were generated using a 1º x 1º spatial grid with the GPlates software \url{https://www.gplates.org} and the full-plate rotation model
-#' "Merdith2021" (Merdith et al., 2021). A finer-scale spatial grid will be implemented in the future, along with the inclusion of more plate rotation models. For now, Merdith2021 is the only model included
-#' as it is the newest model, and one of the few full-plate rotation models available.
+#' The current palaeorotations (0--540 Ma) provided were generated using a 1º x 1º spatial grid with the GPlates software \url{https://www.gplates.org} and three different plate rotation models
+#' "Merdith2021" (Merdith et al., 2021), "Scotese2018" (Scotese & Wright, 2018), and "Wright2013" (Wright et al. 2013). A finer-scale spatial grid will be implemented in the future, along with the inclusion of more plate rotation models.
 #'
 #' @section Reference:
-#' Andrew S. Merdith, Simon E. Williams, Alan S. Collins, Michael G. Tetley, Jacob A. Mulder, Morgan L. Blades,
-#' Alexander Young, Sheree E. Armistead, John Cannon, Sabin Zahirovic, R. Dietmar Müller. (2021).
+#'
+#'
+#' Merdith, A., Williams, S.E., Collins, A.S., Tetley, M.G., Mulder, J.A., Blades, M.L.,
+#' Young, A., Armistead, S.E., Cannon, J., Zahirovic, S.,  Müller. R.D. (2021).
 #' Extending full-plate tectonic models into deep time: Linking the Neoproterozoic and the Phanerozoic.
 #' Earth-Science Reviews 214 (103477). \url{https://doi.org/10.1016/j.earscirev.2020.103477}.
+#'
+#' Scotese, C., & Wright, N. M. (2018). PALEOMAP Paleodigital Elevation Models (PaleoDEMs) for the Phanerozoic. PALEOMAP Project.
+#' \url{https://www.earthbyte.org/paleodem-resource-scotese-and-wright-2018/}.
+#'
+#' Wright, N., Zahirovic, S., Müller, R. D., & Seton, M. (2013). Towards community-driven paleogeographic reconstructions:
+#' integrating open-access paleogeographic and paleobiology data with plate tectonics. Biogeosciences, 10(3), 1529–1541.
+#' \url{https://doi.org/10.5194/bg-10-1529-2013}.
 #' \cr
 #' @section Developer:
 #' Lewis A. Jones
@@ -94,7 +102,7 @@ palaeorotate <- function(x, model = "Merdith2021", uncertainty = FALSE) {
     }
 
     if (uncertainty == TRUE) {
-      rot_age <- colnames(palaeoverse:::Merdith2021)[3:ncol(palaeoverse:::Merdith2021)]
+      rot_age <- colnames(palaeoverseData:::Merdith2021)[3:ncol(palaeoverseData:::Merdith2021)]
       rot_age <- unique(as.numeric(sub(".*_", "", rot_age)))
       #calculate rotation ages for data
       x$rot_age <-
@@ -103,29 +111,29 @@ palaeorotate <- function(x, model = "Merdith2021", uncertainty = FALSE) {
         })]
       #search for matching longitude, latitude and ages
       x$rot_lng <- sapply(1:nrow(x), function(i) {
-        palaeoverse:::Merdith2021[which.min(abs(palaeoverse:::Merdith2021[, c("lng")]  - x$lng[i])), 1] #extract closest longitude
+        palaeoverseData:::Merdith2021[which.min(abs(palaeoverseData:::Merdith2021[, c("lng")]  - x$lng[i])), 1] #extract closest longitude
       }, simplify = TRUE)
 
       x$rot_lat <- sapply(1:nrow(x), function(i) {
-        palaeoverse:::Merdith2021[which.min(abs(palaeoverse:::Merdith2021[, c("lat")]  - x$lat[i])), 2] #extract closest longitude
+        palaeoverseData:::Merdith2021[which.min(abs(palaeoverseData:::Merdith2021[, c("lat")]  - x$lat[i])), 2] #extract closest longitude
       }, simplify = TRUE)
 
       #get coordinates for each model
       Merdith <- data.frame(t(sapply(1:nrow(x), function(i) {
-        as.numeric(palaeoverse:::Merdith2021[which(palaeoverse:::Merdith2021[, c("lng")] == x[i, "rot_lng"] &
-                                          palaeoverse:::Merdith2021[, c("lat")] == x[i, "rot_lat"]),
+        as.numeric(palaeoverseData:::Merdith2021[which(palaeoverseData:::Merdith2021[, c("lng")] == x[i, "rot_lng"] &
+                                                         palaeoverseData:::Merdith2021[, c("lat")] == x[i, "rot_lat"]),
                                   c(paste0("lng_", x$rot_age[i]), paste0("lat_", x$rot_age[i]))])
       })))
 
       Scotese <- data.frame(t(sapply(1:nrow(x), function(i) {
-        as.numeric(palaeoverse:::Scotese2018[which(palaeoverse:::Scotese2018[, c("lng")] == x[i, "rot_lng"] &
-                                          palaeoverse:::Scotese2018[, c("lat")] == x[i, "rot_lat"]),
+        as.numeric(palaeoverseData:::Scotese2018[which(palaeoverseData:::Scotese2018[, c("lng")] == x[i, "rot_lng"] &
+                                                     palaeoverseData:::Scotese2018[, c("lat")] == x[i, "rot_lat"]),
                                   c(paste0("lng_", x$rot_age[i]), paste0("lat_", x$rot_age[i]))])
       })))
 
       Wright <- data.frame(t(sapply(1:nrow(x), function(i) {
-        as.numeric(palaeoverse:::Wright2013[which(palaeoverse:::Wright2013[, c("lng")] == x[i, "rot_lng"] &
-                                         palaeoverse:::Wright2013[, c("lat")] == x[i, "rot_lat"]),
+        as.numeric(palaeoverseData:::Wright2013[which(palaeoverseData:::Wright2013[, c("lng")] == x[i, "rot_lng"] &
+                                                    palaeoverseData:::Wright2013[, c("lat")] == x[i, "rot_lat"]),
                                  c(paste0("lng_", x$rot_age[i]), paste0("lat_", x$rot_age[i]))])
       })))
 
@@ -176,11 +184,11 @@ palaeorotate <- function(x, model = "Merdith2021", uncertainty = FALSE) {
     if (uncertainty == FALSE) {
       #which model should be used?
       if (model == "Merdith2021") {
-        palaeo_rots <- palaeoverse:::Merdith2021
+        palaeo_rots <- palaeoverseData:::Merdith2021
       } else if (model == "Scotese2018") {
-        palaeo_rots <- palaeoverse::Scotese2018
+        palaeo_rots <- palaeoverseData:::Scotese2018
       } else if (model == "Wright2013") {
-        palaeo_rots <- palaeoverse::Wright2013
+        palaeo_rots <- palaeoverseData:::Wright2013
       }
 
       rot_age <- colnames(palaeo_rots)[3:ncol(palaeo_rots)]
