ad5505cae84b4b3610c0e16dda3ae0378ba80da9
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-06-24 15:11:13 +0200

    update to time_bins()
    Update to time_bins() based on Kilian's review. See REVIEW: time_bins() #12 for details.

--- tests/testthat/test-time_bins.R
+++ tests/testthat/test-time_bins.R
@@ -4,9 +4,9 @@ test_that("time_bins() works", {
 
   expect_equal(nrow(time_bins(interval = c("Maastrichtian"))), 1)
 
-  expect_equal(nrow(time_bins(interval = c("Fortunian", "Meghalayan"), equal = TRUE, size = 10)), 50)
+  expect_equal(nrow(time_bins(interval = c("Fortunian", "Meghalayan"), size = 10)), 50)
 
-  expect_equal(nrow(time_bins(interval = c("Fortunian", "Meghalayan"), equal = TRUE, size = 6)), 76)
+  expect_equal(nrow(time_bins(interval = c("Fortunian", "Meghalayan"), size = 6)), 76)
 
   expect_true(is.list(time_bins(interval = c("Fortunian", "Meghalayan"), assign = c(232, 167, 33), plot = TRUE)))
 
@@ -14,7 +14,7 @@ test_that("time_bins() works", {
 
   expect_equal(nrow(time_bins(interval = c("Fortunian", "Holocene"), scale = "GTS2012")), 100)
 
-  expect_equal(nrow(time_bins(interval = c("Fortunian", "Holocene"), scale = "GTS2012", equal = TRUE, size = 10)), 51)
+  expect_equal(nrow(time_bins(interval = c("Fortunian", "Holocene"), scale = "GTS2012", size = 10)), 51)
 
   expect_true(is.list(time_bins(interval = c("Fortunian", "Holocene"), scale = "GTS2012", assign = c(232, 167, 33), plot = TRUE)))
 
@@ -35,6 +35,8 @@ test_that("time_bins() works", {
   expect_error(time_bins(interval = "Mesozoic", plot = "TRUE"))
 
   expect_error(time_bins(interval = "Mesozoic", assign = 40))
+
+  expect_error(time_bins(interval = "Mesozoic", assign = -40))
 
   expect_error(time_bins(interval = "Mesozoic", assign = "test"))
 
