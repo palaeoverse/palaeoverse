33b1726c63fe2eee03989b507bfe6bf0b1ca4a58
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-07-05 10:48:51 +0200

    update to testing

--- tests/testthat/test-time_binning.R
+++ tests/testthat/test-time_binning.R
@@ -33,11 +33,17 @@ test_that("time_binning() works", {
   bins <- data.frame(bin = 1:54, max_ma = seq(10, 540, 10), min_ma = seq(0, 530, 10))
   #tests
   expect_equal(class(time_binning(occdf = occdf, bins = bins, method = "mid")$bin_assignment), "integer")
+
   expect_equal(nrow(time_binning(occdf = occdf, bins = bins, method = "random")), nrow(occdf))
+
   expect_equal(is.list(time_binning(occdf = occdf, bins = bins, method = "point")$point_estimates), TRUE)
-  expect_equal(
-    any(
-      colnames(
-        time_binning(occdf = occdf, bins = bins, method = "majority")) == "overlap_percentage"),
-    TRUE)
+
+  expect_equal(length(time_binning(occdf = occdf, bins = bins, method = "random")$bin_assignment[[1]]), 100)
+
+  expect_equal(any(colnames(time_binning(occdf = occdf, bins = bins, method = "majority")) == "overlap_percentage"), TRUE)
+
+  expect_equal(nrow(time_binning(occdf = occdf, bins = bins, method = "all")) > nrow(occdf), TRUE)
+
+  occdf <- data.frame(max_ma = c("Mastrichtian", "Albian"), min_ma = c("Mastrichtian", "Albian"))
+  expect_equal(is.integer(time_binning(occdf = occdf, bins = bins, return_error = TRUE)), TRUE)
 })
