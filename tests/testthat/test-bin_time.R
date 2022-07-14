07c727e50f6ea3e2d98e21cc9218e421c0b83fb6
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-07-14 19:29:49 +0200

    Function name, output return, sampling method
    - Updated function name
    - Updated to return lists for random and point
    - Updated point method to use uniform sampling

--- tests/testthat/test-bin_time.R
+++ tests/testthat/test-bin_time.R
@@ -1,28 +1,28 @@
-test_that("time_binning() works", {
+test_that("bin_time() works", {
 
   #error handling
-  expect_error(time_binning(occdf = c(50, 20, 10)))
+  expect_error(bin_time(occdf = c(50, 20, 10)))
 
-  expect_error(time_binning(bins = c(50, 20, 10)))
+  expect_error(bin_time(bins = c(50, 20, 10)))
 
-  expect_error(time_binning(occdf = data.frame(),
+  expect_error(bin_time(occdf = data.frame(),
                             bins = c(50, 20, 10)))
 
-  expect_error(time_binning(occdf = data.frame(),
+  expect_error(bin_time(occdf = data.frame(),
                             bins = data.frame(),
                             method = "assign"))
 
-  expect_error(time_binning(occdf = data.frame(),
+  expect_error(bin_time(occdf = data.frame(),
                             bins = data.frame(),
                             method = "mid",
                             reps = TRUE))
 
-  expect_error(time_binning(occdf = data.frame(),
+  expect_error(bin_time(occdf = data.frame(),
                             bins = data.frame(),
                             method = "mid",
                             scale = "2012"))
 
-  expect_error(time_binning(occdf = data.frame(),
+  expect_error(bin_time(occdf = data.frame(),
                             bins = data.frame(),
                             method = "mid",
                             scale = "GTS2020",
@@ -35,38 +35,39 @@ test_that("time_binning() works", {
                      max_ma = seq(10, 540, 10),
                      min_ma = seq(0, 530, 10))
   #tests
-  expect_equal(class(time_binning(occdf = occdf, bins = bins,
+  expect_equal(class(bin_time(occdf = occdf, bins = bins,
                                   method = "mid")$bin_assignment), "integer")
 
-  expect_equal(class(time_binning(occdf = occdf, bins = bins,
+  expect_equal(class(bin_time(occdf = occdf, bins = bins,
                                   scale = "GTS2012",
                                   method = "mid")$bin_assignment), "integer")
 
-  expect_equal(nrow(time_binning(occdf = occdf, bins = bins,
-                                 method = "random")), nrow(occdf))
+  expect_equal(length(bin_time(occdf = occdf, bins = bins,
+                                 method = "random")), 100)
 
-  expect_equal(is.list(time_binning(occdf = occdf, bins = bins,
-                                    method = "point")$point_estimates), TRUE)
+  expect_equal(is.list(bin_time(occdf = occdf, bins = bins,
+                                    method = "point")), TRUE)
 
-  expect_equal(is.list(time_binning(occdf = occdf, bins = bins,
+  expect_equal(is.list(bin_time(occdf = occdf, bins = bins,
                                     reps = 1,
                                     method = "random")$bin_midpoint), FALSE)
 
-  expect_equal(length(time_binning(occdf = occdf,
+  expect_equal(length(bin_time(occdf = occdf,
                                    bins = bins,
-                                   method = "random")$bin_assignment[[1]]), 100)
+                                   method = "random")),
+               100)
 
-  expect_equal(any(colnames(time_binning(
+  expect_equal(any(colnames(bin_time(
     occdf = occdf,
     bins = bins, method = "majority")) == "overlap_percentage"), TRUE)
 
-  expect_equal(nrow(time_binning(occdf = occdf,
+  expect_equal(nrow(bin_time(occdf = occdf,
                                  bins = bins,
                                  method = "all")) > nrow(occdf), TRUE)
 
   occdf <- data.frame(max_ma = c("Mastrichtian", "Albian"),
                       min_ma = c("Mastrichtian", "Albian"))
-  expect_equal(is.integer(time_binning(occdf = occdf,
+  expect_equal(is.integer(bin_time(occdf = occdf,
                                        bins = bins,
                                        return_error = TRUE)), TRUE)
 })
