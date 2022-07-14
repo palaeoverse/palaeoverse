a03defbddff1af20d8eb69aaff9011fb59b6ef69
Bethany Allen <allenb@bs-mbpr540.d.ethz.ch>
2022-07-14 18:23:02 +0200

    Grammar

--- data-raw/tetrapod_data_desc.R
+++ data-raw/tetrapod_data_desc.R
@@ -1,6 +1,7 @@
 # A description of the early tetrapod datasets provided for testing functions:
 
-# Both were initially downloaded from the Paleobiology Database on 14th July 2022
+# Both were initially downloaded from the Paleobiology Database on 14th July
+#  2022.
 # Both datasets are for the clade 'Tetrapoda', covering a time interval from the
 #  Carboniferous to the Early Triassic. These data are relatively well curated
 #  (i.e. complete with respect to the literature).
@@ -10,8 +11,8 @@
 
 # early_tetrapods_min.csv is the semi-cleaned version. It includes only body
 #  fossils, does not include any identifiers which indicate uncertainty (e.g.
-#  cf., aff. ?), and only includes fossils from terrestrial environments.
-# Only columns which are likely to be commonly used in analyses are included.
+#  cf., aff., ?), and only includes fossils from terrestrial environments.
+#  Only columns which are likely to be commonly used in analyses are included.
 
 # early_tetrapods_max.csv is 'hard mode'. It has fossils of all preservation
 #  types, fossils identified with uncertainty, and all preservational
