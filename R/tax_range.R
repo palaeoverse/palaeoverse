e5b930bc0ec03761486f5cde186454b610b8869e
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-07-23 00:22:50 +0100

    add convex hull method

--- R/tax_range.R
+++ R/tax_range.R
@@ -1,48 +1,52 @@
 #' Calculate the range of fossil taxa
 #'
-#' A function to calculate various types of ranges for fossil taxa. The
-#' function can calculate either temporal, (palaeo-)latitudinal, or the spatial
-#' range of taxa.
+#' A function to calculate the range of fossil taxa. The function can calculate
+#' either the temporal, (palaeo-)latitudinal, or the geographic range of taxa.
 #'
-#' @param occdf \code{dataframe}. A dataframe of the fossil occurrences you
-#' wish to calculate range metrics for. For the "temporal" method, the
-#' dataframe should contain at least the following columns: "name"
-#' (e.g., species name), "max_ma", and "min_ma". For the "latitudinal" method,
-#' the dataframe should contain at least the following columns: "name" and
-#' "p_lat" (i.e., palaeolatitude). For the "spatial" method, the dataframe
-#' should contain at least the following columns: "name", "p_lng" and "p_lat".
-#' @param method \code{character}. The type of ranges you wish to calculate
-#' for each unique taxa in the occurrence dataframe. Three options exist in
-#' this function: "temporal", "latitudinal", and "spatial". See Details for a
+#' @param occdf \code{dataframe}. A dataframe of fossil occurrences.
+#' For the "temporal" method, the dataframe should contain at least the
+#' following columns: "name" (e.g., species name), "max_ma", and "min_ma". For
+#' the "lat" method, the dataframe should contain at least the following
+#' columns: "name" and "p_lat" (i.e., palaeolatitude). For the "geo" method,
+#' the dataframe should contain at least the following columns: "name", "p_lng"
+#' and "p_lat" (i.e., palaeolatitude and palaeolongitude).
+#' @param method \code{character}. The type of range you wish to calculate
+#' for each unique taxa in `occdf`. Three options exist in
+#' this function: "temporal", "lat", and "geo". See Details for a
 #' description of each.
 #' @param plot \code{logical}. Should a plot of the ranges be generated?
 #'
 #' @return A \code{dataframe} with method specific columns. For the "temporal"
 #' method, a \code{dataframe} with unique taxa (`name`), taxa ID (`taxa_id`),
 #' maximum age of occurrence (`max_ma`), minimum age of occurrence
-#' (`min_ma`) and range duration (`range_myr`) is returned. For the
-#' "latitudinal" method, a \code{dataframe} with unique taxa (`name`),
+#' (`min_ma`), and range duration (`range_myr`) is returned. For the
+#' "lat" method, a \code{dataframe} with unique taxa (`name`),
 #' taxa ID (`taxa_id`), maximum palaeolatitude of occurrence (`max_p_lat`),
-#' minimum palaeolatitude of occurrence (`min_p_lat`) and palaeolatitudinal
-#' range (`range_p_lat`) is returned.
+#' minimum palaeolatitude of occurrence (`min_p_lat`), and palaeolatitudinal
+#' range (`range_p_lat`) is returned. For the "geo" method, a \code{dataframe}
+#' with unique taxa (`name`), taxa ID (`taxa_id`), convex hull coordinates
+#' (`p_lng` & `p_lat`), and area (`area`) in km^2 is returned.
 #'
 #' @details Three approaches (methods) exist in the `tax_range` function for
 #' calculating ranges:
 #' - Temporal: the "temporal" method calculates the temporal range of a taxa.
 #' It does so by extracting all unique taxa from the input `occdf`, and
-#' checking their first and last appearance. Subsequently, the temporal range
+#' checks their first and last appearance. Subsequently, the temporal range
 #' of each taxa is calculated.
-#' - Latitudinal: the "latitudinal" method calculates the (palaeo-)latitudinal
+#' - Latitudinal: the "lat" method calculates the (palaeo-)latitudinal
 #' range of a taxa. It does so by extracting all unique taxa from the input
-#' `occdf` and checking their most notherly and southerly occurrence.
+#' `occdf` and checking their most northerly and southerly occurrence.
 #' Subsequently, the latitudinal range of each taxa is calculated.
-#' - Spatial:
+#' - Geographic: the "geo" method calculates the geographic range of a taxa. It
+#' does so by generating a convex hull for each taxa in the `occdf`, and
+#' calculates the area of the convex hull (km^2) using
+#' \code{\link[geosphere:areaPolygon]{geosphere::areaPolygon()}}.
 #'
 #' @section Developer(s):
-#' Lewis A. Jones, Bethany Allen & Christopher D. Dean
+#' Lewis A. Jones, Bethany Allen, & Christopher D. Dean
 #' @section Reviewer(s):
 #' To be reviewed
-#'
+#' @importFrom geosphere areaPolygon
 #' @examples
 #' # Grab internal data
 #' occdf <- tetrapods
@@ -66,14 +70,14 @@ tax_range <- function(occdf, method = "temporal", plot = FALSE) {
     stop("`method` should be of character class")
   }
 
-  possible_methods <- c("temporal", "latitudinal", "spatial")
+  possible_methods <- c("temporal", "lat", "geo")
   method_match <- charmatch(method, possible_methods)
 
   if (is.na(method_match) == TRUE) {
     # If the user has entered a non-valid term for the "method" argument,
     # generate an error and warn the user.
     stop("Invalid `method`. Choose either:
-  'temporal', 'latitudinal', or 'spatial'.")
+  'temporal', 'lat', or 'geo'.")
   } else {
     method <- possible_methods[method_match]
   }
@@ -89,9 +93,9 @@ tax_range <- function(occdf, method = "temporal", plot = FALSE) {
     }
   }
 
-  if (method == "latitudinal") {
+  if (method == "lat") {
     if (sum(c("name", "p_lat") %in% colnames(occdf)) != 2) {
-      stop("The 'latitudinal' approach in `method` requires the columns:
+      stop("The 'lat' approach in `method` requires the columns:
          'name', 'p_lat'")
     }
     if (!is.numeric(occdf$p_lat)) {
@@ -160,7 +164,7 @@ tax_range <- function(occdf, method = "temporal", plot = FALSE) {
   }
 
   #=== Latitudinal range ===
-  if (method == "latitudinal") {
+  if (method == "lat") {
     # Generate dataframe for population
     lat_df <- data.frame(name = unique_taxa,
                          taxa_id = seq(1, length(unique_taxa), 1),
@@ -176,11 +180,12 @@ tax_range <- function(occdf, method = "temporal", plot = FALSE) {
     }
     # Remove row names
     row.names(lat_df) <- NULL
-    # Add taxa id
 
-    #round off values
+    # Round off values
     lat_df[, c("max_p_lat", "min_p_lat", "range_p_lat")] <- round(
       x = lat_df[, c("max_p_lat", "min_p_lat", "range_p_lat")], digits = 3)
+
+    # Plot data?
     if (plot == TRUE){
       y_range <- c(min(lat_df$min_p_lat), max(lat_df$max_p_lat))
       x_range <- c(0, nrow(lat_df))
@@ -189,9 +194,9 @@ tax_range <- function(occdf, method = "temporal", plot = FALSE) {
            xlim = x_range,
            ylim = y_range,
            axes = TRUE,
-           ylab = "Palaeolatitude (\u00B0)",
+           ylab = "(Palaeo-)latitude (\u00B0)",
            xlab = "Taxa ID",
-           main = "Palaeolatitudinal range of taxa")
+           main = "(Palaeo-)latitudinal range of taxa")
       segments(y0 = lat_df$max_p_lat,
                y1 = lat_df$min_p_lat,
                x0 = lat_df$taxa_id,
@@ -207,5 +212,46 @@ tax_range <- function(occdf, method = "temporal", plot = FALSE) {
     }
     # Return dataframe
     return(lat_df)
+  }
+  #=== Geographic range ===
+  if (method == "geo") {
+    # Generate dataframe for population
+    spat_df <- data.frame()
+    # Run for loop across unique taxa
+    for (i in seq_along(unique_taxa)) {
+      name <- unique_taxa[i]
+      taxa_id <- i
+      # Subset taxa
+      tmp <- occdf[which(occdf$name == unique_taxa[i]), ]
+      # Calculate convex hull
+      tmp <- tmp[chull(x = tmp$p_lng, y = tmp$p_lat),
+                 c("p_lng", "p_lat")]
+      # Calculate area of convex hull and convert to km^2
+      area <- geosphere::areaPolygon(tmp) / 1e+6
+      tmp <- cbind.data.frame(name, taxa_id, tmp, area)
+      spat_df <- rbind.data.frame(spat_df, tmp)
+    }
+    # Plot data?
+    if (plot == TRUE) {
+      y_range <- c(min(spat_df$p_lat), max(spat_df$p_lat))
+      x_range <- c(min(spat_df$p_lng), max(spat_df$p_lng))
+      plot(x = NA,
+           y = NA,
+           xlim = x_range,
+           ylim = y_range,
+           axes = TRUE,
+           ylab = "(Palaeo-)latitude (\u00B0)",
+           xlab = "(Palaeo-)longitude (\u00B0)",
+           main = "(Palaeo-)geographic range of taxa")
+      for (i in seq_along(unique_taxa)) {
+        tmp <- spat_df[which(spat_df$name == unique_taxa[i]), ]
+        polygon(x = tmp$p_lng, y = tmp$p_lat,
+                col = rgb(red = 0, green = 0, blue = 1, alpha = 0.5))
+        points(tmp[,c("p_lng", "p_lat")], pch = 20)
+      }
+    }
+    # Remove row names
+    row.names(spat_df) <- NULL
+    return(spat_df)
   }
 }
