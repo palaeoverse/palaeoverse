946ff86d676841c9eced88c91a23fe17766dc9fe
Kilian Eichenseer <kilian.eichenseer@gmail.com>
2022-08-30 17:03:43 +0100

    look_up development

--- R/look_up.R
+++ R/look_up.R
@@ -3,7 +3,7 @@
 #' A function to assign fossil occurrences to international geological stages
 #' (GTS2020) based on interval names.
 #'
-#' @param data \code{dataframe}. A dataframe of the fossil occurrences you
+#' @param occdf \code{dataframe}. A dataframe of the fossil occurrences you
 #' wish to bin. This dataframe should contain  the following named columns:
 #' "early_interval" and, optionally, "late_interval". These columns need
 #' to be `character` values. If no "late_interval is supplied, only
@@ -28,34 +28,67 @@
 #' @examples
 #' \dontrun{
 #' #Grab internal tetrapod data
-#' occdf <- tetrapods#'
+#' occdf <- tetrapods
 #' }
 #' @export
-look_up <- function(data, early_interval = NULL, late_interval = NULL) {
+look_up <- function(occdf, early_interval = NULL, late_interval = NULL) {
 
   #=== Handling errors ===
 
-  if (is.data.frame(data) == FALSE) {
-    stop("`data` should be a dataframe.")
+  if (is.data.frame(occdf) == FALSE) {
+    stop("`occdf` should be a dataframe.")
   }
 
-  if (is.null(early_interval) & !(early_interval %in% colnames(data))) {
-    stop("`early_interval` needs to match a column name of `data`, or an
+  if (is.null(early_interval) & !("early_interval" %in% colnames(occdf))) {
+    stop("`early_interval` needs to match a column name of `occdf`, or an
          alternative name for the early interval columns needs to be provided.")
   }
 
   if(!is.null(early_interval)) {
     if(!is.character(early_interval))
       stop("`early_interval` needs to be of type `character`")
-   if(!early_interval %in% colnames(data))
-     stop("`early_interval` needs to match a column name of `data`")
+   if(!early_interval %in% colnames(occdf))
+     stop("`early_interval` needs to match a column name of `occdf`")
   }
 
   if(!is.null(late_interval)) {
     if(!is.character(late_interval))
       stop("`late_interval` needs to be of type `character`")
-    if(!late_interval %in% colnames(data))
-      stop("`late_interval` needs to match a column name of `data`")
+    if(!late_interval %in% colnames(occdf))
+      stop("`late_interval` needs to match a column name of `occdf`")
   }
 
 }
+
+# save early and late int columns for easier handling
+if(is.null(early_interval)) early <- occdf$early_interval else early <-
+    occdf[,early_interval]
+
+if(is.null(late_interval) & "late_interval" %in% colnames(occdf)) late <-
+    occdf$late_interval else { if(!is.null(late_interval)) {
+  late <- occdf[,late_interval]}
+     else late <- NULL }
+
+
+# prepare output
+occdf$early_stage <- rep(NA_character_,nrow(occdf))
+if(!is.null(late)) occdf$late_stage <- rep(NA_character_, nrow(occdf))
+
+#load look-up table from Palaeoverse Onedrive
+id <- "16OWHzbcUyWICDkGafZZ-pvaWDU5mzqDJ"
+key <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id))
+
+
+early_unique <- unique(early)
+assign <- early_unique[early_unique %in% key$interval_name_ori]
+for(i in seq_len(length(assign))){
+  occdf$early_stage[early==]
+}
+
+if(!is.null(late)) late_unique <- unique(late)
+assign <- early_unique[early_unique %in% key$interval_name_ori]
+
+
+occdf <- palaeoverse::tetrapods
+early_interval = NULL
+late_interval = NULL
