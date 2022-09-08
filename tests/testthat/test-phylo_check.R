fedae51e6187b9605ee33edc8f3ea035dbe6635c
Bethany Allen <39991015+bethany-j-allen@users.noreply.github.com>
2022-09-08 13:52:47 +0200

    phylo_check (#37)
    * First draft of phylo_check
    
    * Write example and tests
    
    * Write manual, lint, fix test
    
    * Change syntax
    
    * Add ape dependency
    
    * Add paleotree dependency
    
    * Extra tests
    
    * Update documentation
    
    * Add table sorting
    
    * Update tests for new defaults
    
    * Lint, update documentation
    
    * Move paleotree to Suggests
    
    * Switch default sort
    
    * Add fixRootTime warning, add diff_table
    
    * Update description
    
    Co-authored-by: Bethany Allen <allenb@bs-mbpr540.d.ethz.ch>
    Co-authored-by: Will Gearty <willgearty@gmail.com>

---
+++ tests/testthat/test-phylo_check.R
@@ -0,0 +1,31 @@
+test_that("phylo_check() works", {
+  library(paleotree)
+  data(RaiaCopesRule)
+  tree <- ceratopsianTreeRaia
+  list <- c("Nasutoceratops_titusi", "Diabloceratops_eatoni",
+            "Zuniceratops_christopheri", "Psittacosaurus_major",
+            "Psittacosaurus_sinensis", "Avaceratops_lammersi",
+            "Xenoceratops_foremostensis", "Leptoceratops_gracilis",
+            "Triceratops_horridus", "Triceratops_prorsus")
+
+  #expect equal
+  expect_equal(nrow(phylo_check(tree, list)), 40)
+  expect_equal(nrow(phylo_check(tree, list, out = "diff_table")), 33)
+  expect_equal(nrow(phylo_check(tree, list, out = "counts")), 3)
+  expect_equal(nrow(phylo_check(tree, list = "PsiTTacOsaurUs sinEnsIs",
+                                out = "full_table")), 37)
+
+  #expect true
+  expect_true(is.data.frame(phylo_check(tree, list)))
+  expect_true(is.data.frame(phylo_check(tree, list, out = "counts")))
+  expect_true(inherits(phylo_check(tree, list, out = "tree"), "phylo"))
+
+  #expect error
+  expect_error(phylo_check())
+  expect_error(phylo_check(tree))
+  expect_error(phylo_check(tree = list))
+  expect_error(phylo_check(tree = tree, list = tree))
+  expect_error(phylo_check(tree, list, out = "test"))
+  expect_error(phylo_check(tree, list, sort = "test"))
+  expect_error(phylo_check(tree, list = "."))
+})
