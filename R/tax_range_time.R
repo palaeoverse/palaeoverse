d249e15bd1aa5dbd7db48d4d6da760367778d5b5
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-09-23 11:34:14 +0200

    minor housekeeping

--- R/tax_range_time.R
+++ R/tax_range_time.R
@@ -4,12 +4,12 @@
 #' data.
 #'
 #' @param occdf \code{dataframe}. A dataframe of fossil occurrences. The
-#' dataframe should contain at least three columns: names of taxa (`character`)
-#' and the maximum (`numeric` or `character`) and minimum
-#' (`numeric`  or `character`) age which constrain the
-#' age range (in millions of years if `numeric`) of the fossil occurrence.
-#' If the age range columns are provided as (`character`) values
-#' (i.e., stage names), the function will attempt to extract numeric age data
+#' dataframe should contain at least three columns: names of taxa
+#' and the maximum and minimum age which constrain the
+#' age range of the fossil occurrence. If the age range columns are provided as
+#' `numeric` values, they should be in millions of years before present.
+#' If the age range columns are provided as `character` values
+#' (i.e. stage names), the function will attempt to extract numeric age data
 #' from the Geological Timescale by matching the provided character strings to
 #' the stage names.
 #' @param name \code{character}. The name of the column you wish to be treated
@@ -19,23 +19,22 @@
 #' as the minimum limit of the age range (e.g., "min_ma").
 #' @param max_ma \code{character}. The name of the column you wish to be treated
 #' as the maximum limit of the age range (e.g., "max_ma").
-#' @param by \code{character}. How should the output (and plot) be sorted?
+#' @param by \code{character}. How should the output be sorted?
 #' Either: "FAD" (first-appearance date), "LAD" (last-appearance data), "name"
 #' (alphabetically by taxon names).
 #' @param scale \code{character}. Specify the desired geological timescale to
-#' be used, either "GTS2020" or "GTS2012". This argument is only useful if the
-#' supplied "max_ma" and "min_ma" columns are of class \code{character}.
-#' "GTS2020" is the default. For similar advanced functionality, see
-#' palaeoverse::look_up.
+#' be used, either "GTS2020" (default) or "GTS2012". This argument is only
+#' useful if the supplied "max_ma" and "min_ma" columns are \code{character}
+#' values. For similar advanced functionality, see \code{\link{look_up}}.
 #' @param plot \code{logical}. Should a plot of the ranges be generated?
-#' @param return_error \code{logical}. Should a numeric vector be returned
-#' to flag the rows of the `occdf` that cannot be matched to `character`
-#' interval names? This is only relevant if `max_ma` and `min_ma`
+#' @param return_error \code{logical}. Should a vector be returned
+#' of the interval names of `occdf` that could not be matched to
+#' GTS? This is only relevant if `max_ma` and `min_ma`
 #' are `character` values.
 #'
 #' @return A \code{dataframe} containing the following columns:
 #' unique taxa (`taxa`), taxa ID (`taxon_id`), first appearance of taxon
-#' (`FAD_ma`), last appearance of taxon (`LAD_ma`), duration of temporal
+#' (`max_ma`), last appearance of taxon (`min_ma`), duration of temporal
 #' range (`range_myr`), and number of occurrences per taxon (`n_occ`) is
 #' returned.
 #'
@@ -43,8 +42,8 @@
 #' unique taxa (`name` column) from the input `occdf`, and checking their first
 #' and last appearance. The temporal duration of each taxon is also calculated.
 #' A plot of the temporal range of each taxa is also returned if `plot = TRUE`.
-#' If `return_error == TRUE`, a numeric vector flagging rows that cannot be
-#' matched to interval names is returned.
+#' If `return_error == TRUE`, a vector with interval names that cannot be
+#' matched to stage names is returned.
 #'
 #' Note: this function provides output based solely on the user input data. The
 #' true duration of a taxon is likely confounded by uncertainty in dating
@@ -126,6 +125,7 @@ Either numeric or character, but not both.")
     # If not available, stop and return error
     if (length(vec) != length(unique_intervals)) {
       error_vec <- which(!unique_intervals %in% df$interval_name)
+      error_vec <- unique_intervals[error_vec]
       if (return_error == TRUE) {
         return(error_vec)
       } else {
@@ -133,7 +133,7 @@ Either numeric or character, but not both.")
         paste(c(
           "Check spelling of specified max_ma and min_ma intervals.
   Available interval names are accessible via GTS2020 and GTS2012.
-  Check rows:",
+  Could not extract age information for the following:",
           capture.output(print(error_vec))
         ),
         collapse = "\n"
@@ -211,6 +211,10 @@ Either numeric or character, but not both.")
       axis_geo(side = 1, intervals = "periods")
       title(xlab = "Time (Ma)", line = 4)
     }
+
+    # House keeping
+    colnames(temp_df)[which(colnames(temp_df) == "FAD_ma")] <- c("max_ma")
+    colnames(temp_df)[which(colnames(temp_df) == "LAD_ma")] <- c("min_ma")
 
     # Return dataframe
     return(temp_df)
