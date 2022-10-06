ef3667254fb9723beebdbe6eba8095b458b35148
Will Gearty <willgearty@gmail.com>
2022-10-06 09:39:19 -0400

    Remove h3-info-table

--- data-raw/h3-info-table.R
+++
@@ -1,5 +0,0 @@
-# Generate h3_info_table (not exported from h3jsr package)
-library(devtools)
-library(h3jsr)
-h3_info_table <- h3jsr::h3_info_table
-use_data(h3_info_table, internal = TRUE, overwrite = TRUE)
