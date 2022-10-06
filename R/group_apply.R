8da9b631945358201edcbc157fbdbf4a62c03907
Bethany Allen <allenb@bs-mbpr540.d.ethz.ch>
2022-10-06 17:16:48 +0100

    First draft

--- R/group_apply.R
+++ R/group_apply.R
@@ -1,119 +1,35 @@
-#' Apply a function over grouping(s) of data
+#' Run other functions across subgroups of data
 #'
-#' A function to apply `palaeoverse` functionality across subsets (groups) of
-#' data, delineated using one or more variables. `data.frame` related functions
-#' (e.g. `nrow`, `ncol`, `lengths`, `unique`) may also be used.
+#' A function to run other palaeoverse functions within subsets of data,
+#' delineated using one or more variables.
 #'
-#' @param occdf \code{dataframe}. A dataframe of fossil occurrences.
-#' This dataframe must contain the necessary variables for
-#' whichever `palaeoverse` function you wish to call (see function specific
-#' documentation for required columns).
-#' @param group \code{character}. A vector of column names, specifying the
-#' grouping (e.g. "collection_no", "stage_bin").
-#' @param fun \code{function}. The `palaeoverse` function you wish to apply to
-#' `occdf`. See details for compatible functions.
-#' @param ... Additional arguments available in the called
-#' function. These additional arguments may be required for function arguments
-#' without default values, or if you wish to overwrite the default argument
-#' value (see examples).
+#' @param occdf \code{dataframe}. A dataframe of the fossil occurrences you
+#' wish to analyse. This dataframe must contain the necessary variables for
+#' whichever palaeoverse function you wish to run on it.
+#' @param function_name \code{character}. The function you wish to run on the
+#' dataframe.
+#' @param variables \code{character}. A vector of column names which split the
+#' occurrences into subgroups.
 #'
-#' @return A \code{dataframe} of the outputs from the selected function, with an
-#' appended column indicating the user-defined grouping (`grouping`).
+#' @return A list of outputs from the chosen function, each corresponding to
+#' an individual subgroup of data.
 #'
-#' @details This function is a wrapper which can be used to run other
-#' `palaeoverse` functions on various groupings of data. `data.frame` related
-#' functions (e.g. `nrow`, `ncol`, `lengths`, `unique`) may also be used.
-#' For example, `group_apply` enables the separate analysis of occurrences from
-#' different time intervals, spatial regions, or trait values.
-#' \cr
-#' \cr
-#' Relevant `palaeoverse` functions:
-#' - \code{\link{tax_unique}}: return the number of unique taxa per grouping
-#' variable.
-#' - \code{\link{tax_range_time}}: return the temporal range of taxa per
-#' grouping variable.
-#' - \code{\link{tax_range_geo}}: return the geographic range of taxa per
-#' grouping variable.
-#' - \code{\link{tax_check}}: return potential spelling variations of the
-#' same taxon per grouping variable. Note: `verbose` needs to be set to FALSE.
-#'
+#' @details This function is a wrapper which can be used to run the other
+#' palaeoverse functions on various subgroups of data. For example,
+#' this enables the separate analysis of occurrences from different time
+#' intervals, spatial regions, or trait values.
 #'
 #' @section Developer(s):
-#' Lewis A. Jones & William Gearty
+#' Bethany Allen & William Gearty
 #' @section Reviewer(s):
-#' Kilian Eichenseer & Bethany Allen
-#' @importFrom stats as.formula
+#' William Gearty and Lewis A. Jones
 #' @examples
-#' # Examples
-#' # Get tetrapods data
-#' occdf <- tetrapods
-#' # Remove NA data
-#' occdf <- subset(occdf, !is.na(genus))
-#' # Count number of occurrences from each country
-#' ex1 <- group_apply(occdf = occdf, group = c("cc"), fun = nrow)
-#' # Temporal range of taxa per time bin with default arguments
-#' ex2 <- group_apply(occdf = occdf, group = c("cc"), fun = tax_range_time)
-#' # Temporal range of taxa per time bin with updated arguments
-#' ex3 <- group_apply(occdf = occdf,
-#'                    group = c("cc"),
-#'                    fun = tax_range_time,
-#'                    name = "family") # Run at family level (default: "genus")
-#' # Use multiple variables (number of occurrences per collection and country)
-#' ex4 <- group_apply(occdf = occdf,
-#'                    group = c("collection_no", "cc"),
-#'                    fun = nrow)
+#' #Examples
+#'
 #' @export
-group_apply <- function(occdf, group, fun, ...) {
-
-  # Handle errors
-  if (!is.data.frame(occdf)) {
-    stop("`occdf` should be a dataframe")
-  }
-
-  if (!any(group %in% colnames(occdf))) {
-    stop("Supplied `group` is not a named column in `occdf`")
-  }
-
-  if (!is.function(fun)) {
-    stop("Supplied `fun` is not a function")
-  }
-
-  # Update default arguments with supp arguments
-  supp_args <- list(...)
-  # Which arguments should be updated?
-  indx <- which(names(formals(fun)) %in% names(supp_args))
-  # Which arguments exist in the function?
-  indy <- which(names(supp_args) %in% names(formals(fun)))
-  # Subset supp arguments to those applicable
-  supp_args <- supp_args[indy]
-  # Update order to match function order
-  supp_args <- supp_args[order(match(names(supp_args),
-                                     names(formals(fun)[indx])))]
-  # Update arguments with user-defined input
-  formals(fun)[indx] <- supp_args
-  # Generate bin codes
-  bin_codes <- as.formula(paste0("~ ", paste(group, collapse = " + ")))
-  # Split dataframe
-  lst <- split(occdf, f = bin_codes, drop = TRUE)
-  # Group names
-  nme <- names(lst)
-  # Apply function
-  output <- do.call(rbind, lapply(lst, fun))
-  # String match
-  # Get row names (groups)
-  grouping <- row.names(output)
-  for (i in nme) {
-    vec <- grep(pattern = i, x = grouping, ignore.case = FALSE)
-    grouping[vec] <- i
-  }
-  # Remove funky row names
-  row.names(output) <- NULL
-  # Add group column
-  output <- cbind.data.frame(output, grouping)
-  # Update output if none returned
-  if (nrow(output) == 0) {
-    output <- NULL
-  }
-  # Return output
-  return(output)
+group_apply <- function(occdf, function_name, variables) {
+  form <- as.formula(paste0("~ ", paste(group, collapse = " + ")))
+  lst <- split(tetrapods, f = form, drop = TRUE)
+  results <- do.call(rbind, lapply(lst, function(item) { blah }))
+  return(results)
 }
