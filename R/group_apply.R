7d660b7b585778d44fa3f1b4cfc5c20e9b00d441
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2022-10-11 12:12:34 +0200

    error handling, limit function input

--- R/group_apply.R
+++ R/group_apply.R
@@ -17,7 +17,7 @@
 #' value (see examples).
 #'
 #' @return A \code{dataframe} of the outputs from the selected function, with an
-#' appended column indicating the user-defined grouping (`group`).
+#' appended column indicating the user-defined grouping (`grouping`).
 #'
 #' @details This function is a wrapper which can be used to run other
 #' palaeoverse functions on various groupings of data. For example,
@@ -26,9 +26,14 @@
 #' \cr
 #' \cr
 #' Available compatible `palaeoverse` functions:
-#' - \code{\link{tax_unique}}
-#' - \code{\link{tax_range_time}}
-#' - \code{\link{tax_range_geo}}
+#' - \code{\link{tax_unique}}: return the number of unique taxa per grouping
+#' variable.
+#' - \code{\link{tax_range_time}}: return the temporal range of taxa per
+#' grouping variable.
+#' - \code{\link{tax_range_geo}}: return the geographic range of taxa per
+#' grouping variable.
+#' - \code{\link{tax_check}}: return to potential spelling variations of the
+#' same taxon per grouping variable. Note: `verbose` should be set to FALSE.
 #'
 #'
 #' @section Developer(s):
@@ -55,6 +60,7 @@
 #'             fun = tax_range_time)
 #' @export
 group_apply <- function(df, group, fun, ...) {
+
   # Handle errors
   if (!is.data.frame(df)) {
     stop("`df` should be a dataframe")
@@ -63,6 +69,14 @@ group_apply <- function(df, group, fun, ...) {
   if (!any(group %in% colnames(df))) {
     stop("Supplied `group` is not a named column in `df`")
   }
+
+  fun_name <- deparse(substitute(fun))
+  avail <- c("tax_unique", "tax_range_time", "tax_range_geo", "tax_check")
+  if (!fun_name %in% avail) {
+    stop("Specified function not available or compatible with group_apply.
+    Choose from:", paste(" ", avail))
+  }
+
   # Update default arguments with supp arguments
   supp_args <- list(...)
   indx <- which(names(formals(fun)) %in% names(supp_args))
@@ -74,13 +88,13 @@ group_apply <- function(df, group, fun, ...) {
   # Apply function
   results <- do.call(rbind, lapply(lst, fun))
   # Get row names (groups)
-  group <- row.names(results)
+  grouping <- row.names(results)
   # Clean up group names
-  group <- tools::file_path_sans_ext(group)
+  grouping <- tools::file_path_sans_ext(grouping)
   # Remove funky row names
   row.names(results) <- NULL
   # Add group column
-  results$group <- group
+  results <- cbind.data.frame(results, grouping)
   # Return results
   return(results)
 }
