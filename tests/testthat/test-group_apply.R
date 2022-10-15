5ce40ed7c575b7d2a7505510d91396e68d62c136
Will Gearty <willgearty@gmail.com>
2022-10-15 10:39:56 -0400

    Add nrow test

--- tests/testthat/test-group_apply.R
+++ tests/testthat/test-group_apply.R
@@ -3,11 +3,15 @@ test_that("group_apply works", {
   occdf <- tetrapods
   # Remove NA data
   occdf <- subset(occdf, !is.na(genus))
+  # Number of occurrences from each country
+  expect_equal(nrow(
+    group_apply(occdf = occdf, group = c("cc"), fun = nrow)
+  ), 37)
   # Temporal range of data per time bin with default arguments
   expect_equal(nrow(
     group_apply(occdf = occdf, group = c("cc"), fun = tax_range_time)
     ), 1248)
-  # Temporal range of data per time bin with updated arguments
+  # Temporal range of data per time bin with extra arguments
   expect_equal(nrow(
     group_apply(occdf = occdf,
               group = c("cc"),
