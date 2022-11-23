4c794e645232883e4b9d0af7a9da7333514781d1
willgearty@gmail.com <willgearty@gmail.com>
2022-11-23 15:51:26 -0500

    Remove deeptime use for tests (to prevent future CRAN submission issues)

--- tests/testthat/setup-data.R
+++ tests/testthat/setup-data.R
@@ -1,30 +1,14 @@
 # set up data to test plotting
 if (suppressPackageStartupMessages(require(divDyn, quietly = TRUE))) {
-  stages <- deeptime::stages
-  periods <- deeptime::periods
+  stages <- subset(GTS2020, rank == "stage")
+  periods <- subset(GTS2020, rank == "period")
+  epochs <- subset(GTS2020, rank == "epoch")
   data(corals)
   corals_stages_clean <- subset(corals, stage != "")
   coral_div <- aggregate(cbind(n = genus) ~ stage,
                          data = corals_stages_clean,
                          FUN = function(x) length(x))
   coral_div$stage_age <-
-    (stages$max_age[match(coral_div$stage, stages$name)] +
-       stages$min_age[match(coral_div$stage, stages$name)]) / 2
-
-  coral_div_diet <- aggregate(cbind(n = genus) ~ stage + diet,
-                              data = corals_stages_clean,
-                              FUN = function(x) length(x))
-  coral_div_diet$stage_age <-
-    (stages$max_age[match(coral_div_diet$stage, stages$name)] +
-       stages$min_age[match(coral_div_diet$stage, stages$name)]) / 2
-
-  corals_periods_clean <- subset(corals, period != "")
-  coral_div_dis <- aggregate(cbind(n = genus) ~ period + diet,
-                             data = corals_periods_clean,
-                             FUN = function(x) length(x))
-  coral_div_dis$period_age <-
-    (periods$max_age[match(coral_div_dis$period, periods$name)] +
-       periods$min_age[match(coral_div_dis$period, periods$name)]) / 2
-  coral_div_dis <-
-    coral_div_dis[rev(order(coral_div_dis$period_age)), , drop = FALSE]
+    (stages$max_ma[match(coral_div$stage, stages$interval_name)] +
+       stages$min_ma[match(coral_div$stage, stages$interval_name)]) / 2
 }
--- tests/testthat/test-axis_geo.R
+++ tests/testthat/test-axis_geo.R
@@ -5,18 +5,18 @@ test_that("axis_geo() works", {
     box()
 
     axis(side = 2)
-    axis_geo(side = 1, intervals = "periods")
+    axis_geo(side = 1, intervals = periods)
   })
 })
 
-test_that("axis_geo() works with old GTS scale", {
-  expect_doppelganger("axis_geo() with old GTS scale", function() {
+test_that("axis_geo() works with time_bins()", {
+  expect_doppelganger("axis_geo() with time_bins() scale", function() {
     plot(x = coral_div$stage_age, y = coral_div$n, axes = FALSE,
          xlim = c(250, 0), xlab = NA, ylab = "Diversity")
     box()
 
     axis(side = 2)
-    axis_geo(side = 1, intervals = time_bins(rank = "period"))
+    axis_geo(side = 1, intervals = time_bins(rank = "period", scale = "GTS2020"))
   })
 })
 
@@ -28,7 +28,7 @@ test_that("axis_geo() works with multiple scales", {
     box()
 
     axis(side = 2)
-    axis_geo(side = 1, intervals = list("stages", "periods"),
+    axis_geo(side = 1, intervals = list(stages, periods),
              tick_at = seq(0, 250, 25), lab = list(FALSE, TRUE),
              abbr = FALSE)
   })
@@ -41,17 +41,17 @@ test_that("axis_geo() can be used on multiple sides", {
          xlab = NA, ylab = NA)
     box()
 
-    axis_geo(side = 1, intervals = list("epochs", "periods"),
+    axis_geo(side = 1, intervals = list(epochs, periods),
              height = list(.05, .03), tick_at = seq(0, 100, 25))
     axis_geo(side = 2, height = list(.03, .05),
-             intervals = list("epochs", "periods"), bord_col = "purple",
+             intervals = list(epochs, periods), bord_col = "purple",
              center_end_labels = list(FALSE, TRUE), exact = TRUE)
     axis_geo(side = 3, height = list(.03, .05),
-             intervals = list("epochs", "periods"), abbr = FALSE,
+             intervals = list(epochs, periods), abbr = FALSE,
              skip = c("Paleogene", "Holocene", "Pleistocene", "Pliocene",
                       "Quaternary"), lab_col = list("blue", "purple"))
     axis_geo(side = 4, height = list(.04, .03),
-             intervals = list("epochs", "periods"),
+             intervals = list(epochs, periods),
              fill = list("lightblue", "yellow"),
              lty = list("solid", "dashed"), exact = TRUE, round = 1)
   })
