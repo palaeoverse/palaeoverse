fa21630d03da2992496d7d5fa8a7b050a664262c
Will Gearty <willgearty@gmail.com>
2023-02-17 11:58:04 -0500

    Add get_timescale_data

---
+++ tests/testthat/test-get_timescale_data.R
@@ -0,0 +1,21 @@
+test_that("get_timescale_data works", {
+  expect_named(get_timescale_data(rank = "stage"),
+               c("interval_number", "interval_name", "rank", "max_ma", "mid_ma",
+                 "min_ma", "duration_myr", "font", "colour"))
+  expect_equal(get_timescale_data(rank = "stage"),
+               subset(GTS2020, rank =="stage"))
+  expect_equal(get_timescale_data(name = "GTS2012", rank = "stage"),
+               subset(GTS2012, rank =="stage"))
+  expect_equal(nrow(get_timescale_data(rank = "eon")), 4)
+  expect_equal(get_timescale_data(rank = "stage"),
+               get_timescale_data(rank = "s"))
+
+  expect_error(get_timescale_data(name = "GTS"))
+  expect_error(get_timescale_data(rank = "e"))
+  expect_error(get_timescale_data("international house of pancakes"))
+  expect_named(
+    get_timescale_data("North American Land Mammal Ages"),
+    c("interval_name", "max_ma", "min_ma", "abbr", "colour", "mid_ma",
+      "duration_myr", "font")
+  )
+})
