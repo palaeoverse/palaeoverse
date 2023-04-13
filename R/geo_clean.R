616aaff551bb61a27371784461dda3916396d3a0
Pedro L Godoy <pedrolorenagodoy@gmail.com>
2023-04-13 10:45:41 -0300

    updated geo_clean_development.R

--- R/geo_clean.R
+++ R/geo_clean.R
@@ -1,32 +1,41 @@
-#' Taxonomic spell check
+#' Interval names spell check
 #'
 #' A function to check for and count potential spelling variations of the same
-#' taxon. Spelling variations are checked within alphabetical groups (default),
-#' or within higher taxonomic groups if provided.
+#' interval. ###Spelling variations are checked within alphabetical groups
+#' (default), or within higher taxonomic groups if provided.###
 #'
-#' @param taxdf \code{dataframe}. A dataframe with named columns containing
-#' taxon names (e.g. "species", "genus"). An optional column
+#' @param occdf \code{dataframe}. A dataframe containing
+#' interval names (e.g. "species", "genus"). An optional column
 #' containing the groups (e.g. "family", "order") which taxon names
 #' belong to may also be provided (see `group` for details).
 #' NA values or empty strings in the name and group columns (i.e. "" and " ")
 #' are ignored.
-#' @param name \code{character}. The column name of the taxon names you wish
-#' to check (e.g. "genus").
-#' @param group \code{character}. The column name of the higher taxonomic
-#' assignments in `taxdf` you wish to group by. If `NULL` (default), name
-#' comparison will be conducted within alphabetical groups.
-#' @param dis \code{numeric}. The dissimilarity threshold: a value greater than
-#' 0 (completely dissimilar), and less than 1 (completely similar).
-#' Potential synonyms above this threshold are not returned.
-#' This value is set to 0.05 by default, but the user might wish to experiment
-#' with this value for their specific data.
-#' @param start \code{numeric}. The number of shared characters at the
-#' beginning of potential synonyms that should match. Potential synonyms below
-#' this value will not be returned. By default this value is set to 1 (i.e.
-#' the first letter of synonyms must match).
-#' @param verbose \code{logical}. Should the results of the non-letter
-#' character check be reported to the user? If `TRUE`, the result will only be
-#' reported if such characters are detected in the taxon names.
+#'
+#'
+#' @param column_name \code{character}. The name of the column with the interval
+#' names (e.g. "time_intervals"). ##Can be 1 or 2 columns".##
+#'
+#'
+#' @param reference \code{character} or \code{FALSE}. Allows interval names to
+#' be compared to intervals in the `GTS2020` (default), the `GTS2012` table or
+#' `palaeoverse::interval_key`.
+#'
+#'
+#' @param out \code{character}. Determine whether to return either a
+#' \code{dataframe} describing which taxa are included or not included in the
+#' tree ("full_table", the default), the same table but with taxa included in
+#' both the tree and the list removed ("diff_table"), the counts of taxa
+#' included and not included in the tree ("counts"), or the phylogeny trimmed to
+#' only include taxa in the provided list ("tree").
+#'
+#'
+#' @param clean \code{logical}. Return interval names which could
+#' not be assigned, instead of the dataframe with assignments.
+#' Defaults to \code{FALSE}.
+#'
+#'
+#'
+#'
 #'
 #' @return If verbose = `TRUE` (default), a \code{list} with three elements. The
 #' first element in the list (synonyms) is a \code{data.frame} with each row
@@ -66,9 +75,9 @@
 #' matching. The R Journal 6, 111-122.
 #'
 #' @section Developer(s):
-#' Joseph T. Flannery-Sutherland & Lewis A. Jones
+#' Pedro L. Godoy & Lewis A. Jones
 #' @section Reviewer(s):
-#' Lewis A. Jones, Kilian Eichenseer & Christopher D. Dean
+#' Lewis A. Jones
 #' @importFrom stats na.omit
 #' @importFrom stringdist stringdistmatrix
 #' @examples
@@ -81,8 +90,11 @@
 #'                  group = "family", dis = 0.1)
 #'
 #' @export
-tax_check <- function(taxdf, name = "genus", group = NULL, dis = 0.05,
-                      start = 1, verbose = TRUE) {
+geo_clean <- function(occdf,
+                      column_name = "time_intervals",
+                      reference = "GTS2020",
+                      out = ""
+                      clean = "TRUE") {
 
   # ARGUMENT CHECKS --------------------------------------------------------- #
 
