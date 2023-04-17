d35d65b76704ccaef0bbdc80076812a4592b17e0
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2023-04-17 20:31:30 +0200

    Add setup for prebuilt vignettes

---
+++ vignettes/precompile.R
@@ -0,0 +1,15 @@
+#Vignettes that depend on internet access have been precompiled:
+old_wd <- getwd()
+
+setwd("vignettes/")
+
+library(knitr)
+knit("phanerozoic-reefs.Rmd.orig",
+     "phanerozoic-reefs.Rmd")
+knit("tetrapod-biodiversity.Rmd.orig",
+     "tetrapod-biodiversity.Rmd")
+
+library(devtools)
+build_vignettes()
+
+setwd(old_wd)
