415dfa547fba42512a8c032ff23eeb9446baeab8
Bethany Allen <bethany.allen@bsse.ethz.ch>
2023-11-21 15:14:20 +0100

    Error tests

--- R/plot_section.R
+++ R/plot_section.R
@@ -86,6 +86,10 @@ plot_section <- function (occdf, name = "taxon", level = "bed",
     stop("The `level` column contains NA values")
   }
 
+  if (certainty != FALSE && any(is.na(occdf[, certainty]))) {
+    stop("The `certainty` column contains NA values")
+  }
+
   if (!by %in% c("name", "FAD", "LAD")) {
     stop("`by` must be either \"FAD\", \"LAD\", or \"name\"")
   }
@@ -108,6 +112,7 @@ plot_section <- function (occdf, name = "taxon", level = "bed",
     occ_filter <- occdf[(occdf[, name] == unique_taxa[i]),]
     ranges[i,2] <- min(occ_filter[level])
     ranges[i,3] <- max(occ_filter[level])
+    #If uncertainty is used, create second set of columns for certain IDs
     if (certainty != FALSE) {
       occ_filter <- occ_filter[(occ_filter[, certainty] == 1),]
       if (nrow(occ_filter) == 0) {occ_filter[1,] <- NA}
---
+++ tests/testthat/test-plot_section.R
@@ -0,0 +1,22 @@
+test_that("plot_section() works", {
+
+  occdf <- data.frame(taxon = c("shrimp", "worm", "worm", "shrimp", "bivalve",
+                                "bivalve", "shrimp", "anemone", "worm"),
+                      bed = c(1, 1, 2, 2, 2, 3, 3, 4, 4),
+                      certainty = c(1, 1, 0, 1, 0, 1, 1, 1, 0))
+
+  # Expect error
+  expect_error(plot_section(occdf = NA))
+  expect_error(plot_section(occdf = occdf, name = "test"))
+  expect_error(plot_section(occdf = occdf, level = "test"))
+  expect_error(plot_section(occdf = occdf, level = "taxon"))
+  expect_error(plot_section(occdf = occdf, certainty = "test"))
+  expect_error(plot_section(occdf = occdf, by = "test"))
+
+  occdf[1,1] <- NA;
+  expect_error(plot_section(occdf = occdf))
+  occdf[1,1] <- "shrimp"; occdf[1,2] <- NA
+  expect_error(plot_section(occdf = occdf))
+  occdf[1,2] <- 1; occdf[1,3] <- NA
+  expect_error(plot_section(occdf = occdf))
+})
