e7396033bfbdc5f2b8c2f377b8855c20cf9bab27
Bruna Farina <82400814+BrunaFarina@users.noreply.github.com>
2024-11-29 18:01:59 +0100

    Update tex_certainty function

--- R/tax_certainty.R
+++ R/tax_certainty.R
@@ -1,33 +1,89 @@
-#' Check if a taxon is certain or uncertain.
-#'
+#' Classify the certainty of taxonomic identifications
+
 #' Check whether a given taxon in a vector is certain or uncertain.
-#' Binomial names (genus and species) are considered "certain",
-#' whereas names containing only the genus,
-#' or genus and not determined species is considered "uncertain".
+#'The function screens for "cf.", "aff.", "?", "indet.", "sp.", "ex gr.",
+#'"incertae sedis", "problematica", and NA.
 #'
-#' @param taxon_names Character vector containing the species names to be
-#' checked.
-#' It allows taxon names separated by underscore or space.
-#' @return Numeric vector classifying each species as certain (1) or uncertain
-#' (0).
+#' @param taxon_df \code{data.frame}. A dataframe containing the taxa names
+#' to be checked. It allows taxon names separated by underscore or space
+#' @param name \code{character}. The column name of the taxon names
+#' you wish to check
+#' @param rank \code{character}. Taxonomic rank to be considered when screening
+#' for certainty (species, genus, family, order, class). If no taxonomic rank
+#' provided (e.g., unranked clade), the function will screen for all
+#' previously mentioned qualifiers
+#' @param certainty \code{character}. Vector of how to represent certainty and
+#' uncertainty. Options are TRUE/FALSE, 0/1, or certain/uncertain (default)
+#' @param add_df \code{logical}. If TRUE (default), it adds a "certainty" column
+#' to the input dataframe
+#'
+#' @return Character vector classifying each species as "certain" or "uncertain"
+#'
+#' @section Developer(s):
+#'     Bruna M. Farina
+#' @section Reviewer(s):
+#'     Lewis A. Jones
+#'     Bethany J. Allen
+#'
 #' @examples
-#' sirenia_sp <- c("Metaxytherium indet.", "Felsinotherium_serresii",
-#'                  "Felsinotherium gervaisi", "Potamosiren indet.",
-#'                  "Potamosiren", "Metaxytherium sp.")
+#' data(tetrapods)
 #'
-#' certainty <- tax_certainty(sirenia_sp)
+#' certainty <- tax_certainty(taxon_df = tetrapods name = "identified_name", rank = species,
+#'                           add_df = TRUE)
 #'
 #' @export
-tax_certainty <- function(taxon_names) {
+tax_certainty <- function(taxon_df = NULL, name = NULL, rank = NULL,
+                          certainty = c("certain", "uncertain"),
+                          add_df = TRUE)
 
-  if (is.null(taxon_names) || !is.character(taxon_names) ||
-        length(taxon_names) == 0 || any(is.na(taxon_names))) {
-    stop("'taxon_names' must be a non-empty character vector, cannot be NULL,
-         and must not contain NA values")
+
+  if (!data.frame(taxon_df)) {
+    stop("'taxon_df' must be a data.frame")
   }
-  match_patterns <- c("cf.", "aff.", "\\?", "^[A-Za-z]*$", "indet.", "sp.")
-  certainty <- apply(sapply(match_patterns, grepl, taxon_names), 1, any)
-  certainty <- ifelse(certainty, 0, 1)
 
+if (is.null(taxon_df[,taxon_column]) ||
+    !is.character(taxon_df[,taxon_column]) ||
+    length(taxon_df[,taxon_column]) == 0) {
+  stop("'names' must be a non-empty character vector and cannot be NULL")
+}
+
+
+if (rank == "species") {
+  match_patterns <- c("\\S+\\s+\\cf\\.\\s+\\S+", "\\s+\\S+\\aff\\.\\s+\\S+",
+                      "\\s+\\S+\\?\\s+\\S+", "sp\\.$", "informal", "ex gr\\.",
+                      "indet\\.", "incertae sedis","problematica",  NA)
+  classif  <- apply(sapply(match_patterns, grepl, taxon_df[,taxon_column]),
+                    1, any)
+
+} else if (rank == "genus") {
+  match_patterns <- c("cf\\.\\s*\\S+", "aff\\.\\s*\\S+", "\\?\\s*\\S+", NA)
+  classif <- apply(sapply(match_patterns, grepl, taxon_df[,taxon_column]),
+                   1, any)
+} else if (rank == "family" | "order" | "class") {
+  match_patterns <- c("cf\\.\\s*\\S+", "aff\\.\\s*\\S+", "\\?\\s*\\S+",
+                      "invertae sedis", "problematica", NA)
+  classif <- apply(sapply(match_patterns, grepl, taxon_df[,taxon_column]),
+                   1, any)
+} else {
+  match_patterns <- c("\\S+\\s+\\cf\\.\\s+\\S+", "cf\\.\\s*\\S+",
+                      "\\s+\\S+\\aff\\.\\s+\\S+", "aff\\.\\s*\\S+",
+                      "\\s+\\S+\\?\\s+\\S+","\\?\\s*\\S+",
+                      "sp\\.$", "informal", "ex gr\\.", "indet\\.",
+                      "incertae sedis","problematica",  NA)
+  classif <- apply(sapply(match_patterns, grepl, taxon_df[,taxon_column]),
+                   1, any)
+}
+
+if (certainty == c("certain", "uncertain")) {
+  classif <- ifelse(classif, "certain", "uncertain")
+} else if (certainty == c("TRUE", "FALSE")) {
+  classif <- ifelse(classif, TRUE, FALSE)
+} else if (certainty == c("0", "1")) {
+  classif <- ifelse(classif, 0, 1)
+}
+
+if (add_df == TRUE) {
+  taxon_dfdf['certainty'] <- certainty
+} else {
   return(certainty)
 }
