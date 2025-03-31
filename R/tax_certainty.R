5fa2e6f4db7aab4649c495680715818b6685df1a
Lewis A. Jones <41071747+LewisAJones@users.noreply.github.com>
2025-03-31 16:27:26 +0100

    Suggested edits

--- R/tax_certainty.R
+++ R/tax_certainty.R
@@ -1,126 +1,108 @@
 #' Classify the certainty of taxonomic identifications
-
-#' Check whether a given taxon in a vector is certain or uncertain.
-#'The function screens for 'cf.', 'aff.', '?', 'indet.', 'sp.', 'ex gr.',
-#''incertae sedis', 'problematica', 'informal', and NA.
 #'
-#' @param taxa_df \code{data.frame}. A dataframe containing the taxa names
-#' to be checked. It allows taxon names separated by underscore or space
-#' @param name \code{character}. The column name of the taxon names
-#' you wish to check
-#' @param rank \code{character}. Taxonomic rank to be considered when screening
-#' for certainty (species, genus, family, order, class).
-#' @param certainty \code{character}. Vector of how to represent certainty and
-#' uncertainty. Options are 'TRUE' (uncertain)/'FALSE' (certain),
-#' '0' (certain)/'1' (uncertain), or 'certain'/'uncertain' (default)
-#' @param add_df \code{logical}. If TRUE (default), it adds a 'certainty' column
-#' to the input dataframe
+#' Check whether a given taxonomic name is certain or uncertain by screening
+#' for common substitutes, abbreviations, qualifiers, and notations for
+#' denoting the certainty of taxonomic identifications (see Details for
+#' screening values).
 #'
-#' @return A character vector classifying each species as 'certain' or
-#' 'uncertain', or a new column added to the input dataframe (default).
+#' @param taxdf \code{data.frame}. A dataframe with named columns
+#'   containing taxonomic names to be checked (e.g. "species"). Names
+#'   separated by underscores or spaces are allowed.
+#' @param name \code{character}. The column name of the taxonomic names you
+#'   wish to check (e.g. "genus").
+#' @param rank \code{character}. Taxonomic rank to be considered when
+#'   screening for certainty in taxonomic assignment (e.g. "species", "genus",
+#'   "family", "order", "class").
+#' @param certainty \code{character}. A vector of length two denoting
+#'   how certainty should be coded. The first element of the vector denotes
+#'   "certain" status (default: 1), while the second denotes "uncertain"
+#'   status (default: 0).
+#' @param add_df \code{logical}. If \code{TRUE} (default), a 'certainty'
+#'   column is appended to the input \code{taxdf}. Otherwise, a vector of
+#'   taxonomic identification certainty status is returned.
+#'
+#' @return The input \code{taxdf} with an appended 'certainty' column
+#'   classifying each taxon (default), or a vector if \code{add_df} is
+#'   `FALSE`.
+#'
+#' @details This function screens for common substitutes, abbreviations,
+#'   qualifiers, and notations for denoting the certainty of taxonomic
+#'   identifications.
+#'
+#'   Screened values when `rank` is "species":
+#'   - "cf.", "aff.", "?", "indet.", "sp.", "ex gr.", "incertae sedis",
+#'   "problematica", "informal", and "NA"/NA.
+#'
+#'   Screened values when `rank` is any higher taxonomic rank (e.g. "genus"):
+#'   - "cf.", "aff.", "?", "incertae sedis", "problematica", "informal",
+#'   and "NA"/NA.
 #'
 #' @section Developer(s):
 #'     Bruna M. Farina
 #' @section Reviewer(s):
-#'     Lewis A. Jones
-#'     Bethany J. Allen
+#'     Lewis A. Jones, Bethany J. Allen, & William Gearty
 #'
 #' @examples
+#' # Get internal data
 #' data(tetrapods)
-#'
-#' tetrapods_sp <- tetrapods[which(tetrapods$identified_rank == "species"),]
-#'
-#' certainty <- tax_certainty(taxa_df = tetrapods_sp, name = "identified_name",
-#'                           certainty = c("certain", "uncertain"),
-#'                           rank = "species", add_df = TRUE)
-#'
+#' # Summarise taxonomic certainty
+#' certainty <- tax_certainty(taxdf = tetrapods, name = "accepted_name",
+#'                            certainty = c("certain", "uncertain"),
+#'                            rank = "species", add_df = TRUE)
 #' @export
-tax_certainty <- function(taxa_df = NULL, name = NULL, rank = NULL,
-                          certainty = c("certain", "uncertain"),
+tax_certainty <- function(taxdf = NULL, name = NULL, rank = NULL,
+                          certainty = c(1, 0),
                           add_df = TRUE) {
-
-  if (!is.data.frame(taxa_df)) {
-      stop("'taxon_df' must be a data.frame")
+  # Error handling
+  # Check taxdf is dataframe
+  if (!is.data.frame(taxdf)) {
+      stop("`taxdf` must be a data.frame")
   }
-
-  if (is.null(taxa_df[[name]]) ||
-      !is.character(taxa_df[[name]]) ||
-      length(taxa_df[[name]]) == 0) {
-    stop("'names' must be a non-empty character vector and cannot be NULL")
+  # Check for valid name input
+  if (is.null(taxdf[[name]]) ||
+      !is.character(taxdf[[name]]) ||
+      length(taxdf[[name]]) == 0) {
+    stop("`names` must be a non-empty character vector and cannot be NULL")
   }
-
-  if (!rank %in% c("species", "genus", "family", "order", "class")) {
-    stop("Invalid 'rank'. It must be one of 'species', 'genus', 'family',
-    'order', or 'class'")
+  # Check for valid rank input
+  if (is.null(rank) ||
+      !rank %in% c("species", "genus", "family", "order", "class")) {
+    stop("Invalid `rank`. It must be either: 'species', 'genus', 'family',
+         'order', or 'class'")
   }
-
-  if (is.null(rank)) {
-    stop("Invalid 'rank'. It must be one of 'species', 'genus', 'family',
-    'order', or 'class'")
-  }
-  if (!certainty[1]  %in% c("certain", "uncertain", "TRUE",
-                          "FALSE", "0", "1")) {
-    stop("Invalid 'certainty' option. Must be 'certain'/'uncertain',
-        'TRUE'/'FALSE', or '0'/'1'")
-  }
-
-  rank <- ifelse(rank %in% c("genus", "family", "order", "class"), "other",
-                 "species")
-
-  taxa_df[[name]] <- gsub("^$|^\\s+$", NA, taxa_df[[name]])
-
+  # Assign rank for handling
+  rank <- ifelse(rank == "species", "species", "other")
+  # Create temporary taxdf to not replace original values
+  tmpdf <- taxdf
+  # Replace empty rows with NA
+  tmpdf[[name]] <- gsub(pattern = "^$|^\\s+$", replacement = "NA",
+                        x = tmpdf[[name]])
+  # Change NA to character
+  tmpdf[[name]][which(is.na(tmpdf[[name]]))] <- "NA"
+  # Define match patterns
   match_patterns <- switch(rank,
                           "species" = c("\\S+\\s+cf\\.\\s+\\S+",
                                         "\\S+\\s+aff\\.\\s+\\S+",
                                         "\\S+\\s*\\?\\s*\\S+", "sp\\.$",
                                         "informal","\\S+\\s+ex gr\\.\\s+\\S+",
                                         "indet\\.","incertae sedis",
-                                        "problematica"),
-
+                                        "problematica", "NA"),
                           "other" = c("^\\cf\\.\\s*\\S+", "^\\aff\\.\\s*\\S+",
                                       "^\\?\\S*", "incertae sedis",
-                                      "problematica"))
-
-
-  if (rank == "species") {
-    matches <- sapply(match_patterns, grepl, taxa_df[[name]], ignore.case = TRUE)
-
-    if (is.vector(matches)) {
-      matches <- matrix(matches, nrow = 1, byrow = TRUE)
-    }
-    na_entries <- is.na(taxa_df[[name]])
-    classif <- rowSums(matches) > 0
-
-  } else if (rank == "other") {
-    matches <- sapply(match_patterns, grepl, taxa_df[[name]], ignore.case = TRUE)
-
-    if (is.vector(matches)) {
-      matches <- matrix(matches, nrow = 1, byrow = TRUE)
-    }
-    na_entries <- is.na(taxa_df[[name]])
-    classif <- rowSums(matches) > 0
-
-  }
-
-
-  if ((certainty[1] == "certain" || certainty[1] =="uncertain")
-      & (certainty[2] == "certain" || certainty[2] == "uncertain")) {
-    classif <- ifelse(classif, "uncertain", "certain")
-
-  } else if ((certainty[1] == "TRUE" || certainty[1] == "FALSE") &
-             (certainty[2] == "TRUE" ||certainty[2] == "FALSE")) {
-    classif <- ifelse(classif, TRUE, FALSE)
-
-  } else if ((certainty[1] == "0" || certainty[1] == "1") &
-             (certainty[2] == "0" || certainty[2] == "1")) {
-    classif <- ifelse(classif, 1, 0)
-  }
-
+                                      "problematica", "NA"))
+  # Identify taxonomic certainty
+  matches <- sapply(match_patterns, grepl, tmpdf[[name]], ignore.case = TRUE)
+  # Calculate row sums (1 = match/uncertain, 0 = no match/certain)
+  matches <- rowSums(matches)
+  # Match user definitions
+  # first element of `certainty` is certain (0 + 1), second uncertain (1 + 1)
+  matches <- matches + 1
+  classif <- certainty[matches]
+  # Add to dataframe?
   if (add_df == TRUE) {
-    taxa_df$certainty <- classif
-    return(taxa_df)
+    taxdf$certainty <- classif
+    return(taxdf)
   } else {
     return(classif)
   }
-
 }
--- tests/testthat/test-tax_certainty.R
+++ tests/testthat/test-tax_certainty.R
@@ -1,50 +1,46 @@
-library(testthat)
-
-data(tetrapods)
-tetrapods_sp <- tetrapods[which(tetrapods$identified_rank == "species"),]
-tetrapods_sp <- tetrapods_sp[1:15,]
-
-tetrapods_fam <- tetrapods[which(tetrapods$identified_rank == "family"),]
-tetrapods_fam <- tetrapods_fam[1:15,]
-
-
-test_that("Check certainty", {
-
+test_that("tax_certainty() works", {
+  data("tetrapods")
+  # Error handling
   expect_error(tax_certainty(vector()))
   expect_error(tax_certainty(NULL))
-  expect_error(tax_certainty(taxa_df = tetrapods_sp$identified_name,
+  expect_error(tax_certainty(taxdf = tetrapods$identified_name,
                certainty = c("certain", "uncertain"), rank = "species",
                add_df = FALSE))
-
-  expect_equal(tax_certainty(taxa_df = tetrapods_sp, name = "identified_name",
-                             certainty = c("1", "0"), rank = "species",
-                             add_df = FALSE), c(0, 0, 0, 0, 0, 0, 0, 0,0,
-                                                0, 0, 0, 1, 1, 1))
-  expect_equal(tax_certainty(taxa_df = tetrapods_sp, name = "identified_name",
-                             certainty = c("TRUE", "FALSE"), rank = "species",
-                             add_df = FALSE), c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE,
-                                                FALSE, FALSE, FALSE, TRUE, TRUE, TRUE))
-  expect_equal(tax_certainty(taxa_df = tetrapods_fam, name = "identified_name",
-                                           certainty = c("certain", "uncertain"), rank = "family",
-                                           add_df = FALSE), c("certain", "certain", "certain", "certain",
-                                         "certain", "certain","certain", "certain","certain",
-                                         "certain","certain", "certain", "certain", "certain",
-                                         "certain"))
-
-  ## test if it's correctly adding a column to dataframe
-  res <- tax_certainty(taxa_df = tetrapods_sp, name = "identified_name",
-                             certainty = c("1", "0"), rank = "species",
-                             add_df = TRUE)
-
-  expect_s3_class(res, "data.frame")
-  expect_equal(nrow(res), nrow(tetrapods_sp))
-  expect_equal(ncol(res), ncol(tetrapods_sp) + 1)
-  expect_true("certainty" %in% colnames(res))
-  expect_equal(res$certainty, c(0, 0, 0, 0, 0, 0, 0, 0,0,
-                                   0, 0, 0, 1, 1, 1))
-
-
-
+  expect_error(tax_certainty(taxdf = tetrapods,
+                             name = "accepted_name",
+                             rank = "subspecies",
+                             add_df = FALSE))
+  expect_error(tax_certainty(taxdf = tetrapods,
+                             name = "subspecies",
+                             rank = "species",
+                             add_df = FALSE))
+  # Expect true
+  ## Vector returned
+  expect_true(is.vector(tax_certainty(taxdf = tetrapods,
+                                      name = "accepted_name",
+                                      rank = "species",
+                                      add_df = FALSE)))
+  ## Dataframe returned
+  expect_true(is.data.frame(tax_certainty(taxdf = tetrapods,
+                                          name = "accepted_name",
+                                          rank = "species",
+                                          add_df = TRUE)))
+  ## Custom certainty
+  expect_true(is.logical(tax_certainty(taxdf = tetrapods,
+                                       name = "accepted_name",
+                                       rank = "species",
+                                       certainty = c(TRUE, FALSE),
+                                       add_df = FALSE)))
+  ## Certainty appended correctly
+  expect_true("certainty" %in% colnames(tax_certainty(taxdf = tetrapods,
+                                                      name = "accepted_name",
+                                                      rank = "species",
+                                                      add_df = TRUE)))
+  ## Genus level working?
+  expect_true(is.vector(tax_certainty(taxdf = tetrapods,
+                                      name = "genus",
+                                      rank = "genus",
+                                      add_df = FALSE)))
 })
 
 
