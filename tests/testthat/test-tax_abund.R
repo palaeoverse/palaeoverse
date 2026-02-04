64d0dcefc4cca7da5a0cc4e2cd4458becc9da91a
mcnikolic <mc.nikolic@icloud.com>
2026-02-03 17:45:48 -0800

    test-tax_abund() lintr

--- tests/testthat/test-tax_abund.R
+++ tests/testthat/test-tax_abund.R
@@ -9,7 +9,8 @@ test_that("tax_abund() works", {
 
   occdf <- tetrapods[!is.na(tetrapods$genus),]
   expect_error(tax_abund(occdf = occdf, plot = true))
-  expect_error(tax_abund(occdf, plot_args = c(log = TRUE, col = "pink", brks = 50)))
+  expect_error(tax_abund(occdf, plot_args = c(log = TRUE, col = "pink",
+                                              brks = 50)))
   expect_error(tax_abund(occdf, plot_args = list(log = TRUE, col = pink)))
   expect_error(tax_abund(occdf = occdf, name = "Genus", plot = TRUE))
   expect_error(tax_abund(occdf = occdf, abund_vals = c(1, 20, 1001)))
@@ -19,10 +20,14 @@ test_that("tax_abund() works", {
 
   expect_true(is.data.frame(tax_abund(occdf)))
 
-  expect_equal(sort(tax_abund(occdf, abund_vals = "abund_value")$taxon), sort(unique(occdf$genus)))
+  expect_equal(sort(tax_abund(occdf, abund_vals = "abund_value")$taxon),
+               sort(unique(occdf$genus)))
 
   occdf <- subset(tetrapods, !is.na(family))
-  expect_equal(nrow(tax_abund(occdf = occdf, name = "family")), length(unique(occdf$family)))
-  expect_equal(sort(tax_abund(occdf, name = "family", abund_vals = "abund_value")$taxon), sort(unique(occdf$family)))
+  expect_equal(nrow(tax_abund(occdf = occdf, name = "family")),
+               length(unique(occdf$family)))
+  expect_equal(sort(tax_abund(occdf, name = "family",
+                              abund_vals = "abund_value")$taxon),
+               sort(unique(occdf$family)))
 
 })
