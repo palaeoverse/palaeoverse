2acb095f8b3aee7b0bc67c489161e56587fa62f5
etiennebacher <etienne.bacher@protonmail.com>
2026-05-18 09:02:11 +0100

    restore `release_questions()` with a `jarl-ignore`

---
+++ R/release.R
@@ -0,0 +1,6 @@
+# jarl-ignore unused_function: this is used in usethis::use_release_issue
+release_questions <- function() {
+  c(
+    "Did you re-build the vignettes using `vignettes/precompile.R`?"
+  )
+}
