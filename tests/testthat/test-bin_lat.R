841ddd7ab184da61207cd16538506cb7b4cee1b9
etiennebacher <etienne.bacher@protonmail.com>
2026-05-21 15:34:28 +0100

    re-apply changes

--- tests/testthat/test-bin_lat.R
+++ tests/testthat/test-bin_lat.R
@@ -26,40 +26,41 @@ test_that("bin_lat error handling", {
   occdf <- tetrapods
   # Generate latitudinal bins
   bins <- lat_bins_degrees(size = 10)
-  # Bin data
-  expect_equal(
-    nrow(bin_lat(occdf = occdf, bins = bins, lat = "lat")),
-    nrow(occdf)
-  )
 
-  # Boundary occurrences
-  bo <- length(which(occdf[, "lat", drop = TRUE] %in% c(bins$max, bins$min)))
-  expect_equal(
-    nrow(bin_lat(occdf = occdf, bins = bins, lat = "lat", boundary = TRUE)),
-    nrow(occdf) + bo
-  )
-
-  # Expect error
+  # occdf and bins should be dataframes
   expect_snapshot(bin_lat(occdf = 2, bins = bins, lat = "lat"), error = TRUE)
-  expect_snapshot(bin_lat(occdf = occdf, bins = 2, lat = "lat"), error = TRUE)
-  expect_snapshot(bin_lat(occdf = occdf, bins = 2, lat = "plat"), error = TRUE)
-  bins <- bins[, -c(1)]
   expect_snapshot(
-    bin_lat(occdf = occdf, bins = bins, lat = "lat"),
+    bin_lat(occdf = occdf, bins = 2, lat = "lat"),
     error = TRUE
   )
+
+  # column "lat" must exist in the data
+  expect_snapshot(
+    bin_lat(occdf = occdf, bins = bins, lat = "plat"),
+    error = TRUE
+  )
+
+  # columns "bin", "max", and "min" must exist in bins
+  for (i in c("bin", "min", "max")) {
+    bins2 <- bins
+    bins2[[i]] <- NULL
+    expect_snapshot(
+      bin_lat(occdf = occdf, bins = bins2, lat = "lat"),
+      error = TRUE
+    )
+  }
+
+  # lat cannot have missing values
   occdf$lat[1] <- NA
   expect_snapshot(
     bin_lat(occdf = occdf, bins = bins, lat = "lat"),
     error = TRUE
   )
+
+  # lat must be between -90 and 90
   occdf$lat[1] <- 91
   expect_snapshot(
     bin_lat(occdf = occdf, bins = bins, lat = "lat"),
-    error = TRUE
-  )
-  expect_snapshot(
-    bin_lat(occdf = occdf, bins = bins, lat = "latitude"),
     error = TRUE
   )
 })
--- tests/testthat/test-bin_space.R
+++ tests/testthat/test-bin_space.R
@@ -2,19 +2,22 @@ test_that("bin_space() works", {
   # Reduce data size for faster testing
   occdf <- head(tetrapods, n = 100)
 
-  # Expect equal
-  expect_equal(
-    nrow(bin_space(occdf = tetrapods, spacing = 250, plot = TRUE)),
-    nrow(tetrapods)
+  # We don't lose or gain observations
+  expect_message(
+    expect_equal(
+      nrow(bin_space(occdf = occdf, spacing = 250, plot = TRUE)),
+      nrow(occdf)
+    ),
+    "H3 resolution: 2"
   )
-  expect_equal(
-    nrow(bin_space(
-      occdf = tetrapods,
-      spacing = 1000,
-      sub_grid = 250,
-      plot = TRUE
-    )),
-    nrow(tetrapods)
+  expect_message(
+    expect_equal(
+      nrow(
+        bin_space(occdf = occdf, spacing = 1000, sub_grid = 250, plot = TRUE)
+      ),
+      nrow(occdf)
+    ),
+    "H3 resolution: 1"
   )
 
   # Check output type
@@ -28,46 +31,52 @@ test_that("bin_space() works", {
   expect_message(
     expect_type(
       bin_space(
-        occdf = tetrapods,
-        spacing = 250,
-        return = TRUE,
-        plot = TRUE
-      )
-    )
-  )
-
-  expect_true(
-    is.list(
-      bin_space(
-        occdf = tetrapods,
+        occdf = occdf,
         spacing = 500,
         sub_grid = 200,
         return = TRUE,
         plot = TRUE
-      )
-    )
+      ),
+      "list"
+    ),
+    "H3 resolution: 1"
   )
+})
 
-  # Error handling
+test_that("bin_space error handling", {
+  # We modify this data so copy it first
+  occdf <- tetrapods
+
+  # wrong input type
   expect_snapshot(bin_space(occdf = matrix(tetrapods)), error = TRUE)
-  expect_snapshot(
-    bin_space(occdf = tetrapods, lng = "long", lat = "latit"),
-    error = TRUE
-  )
-  expect_snapshot(
-    bin_space(occdf = tetrapods, spacing = 1000, sub_grid = 1000),
-    error = TRUE
-  )
   expect_snapshot(bin_space(occdf = tetrapods, spacing = NA), error = TRUE)
   expect_snapshot(
     bin_space(occdf = tetrapods, spacing = 1000, sub_grid = NA),
     error = TRUE
   )
   expect_snapshot(bin_space(occdf = tetrapods, return = "TRUE"), error = TRUE)
-  tetrapods$lat[1] <- 94
-  expect_snapshot(bin_space(occdf = tetrapods), error = TRUE)
-  tetrapods$lat[1] <- "94"
-  expect_snapshot(bin_space(occdf = tetrapods), error = TRUE)
-  tetrapods$lng[1] <- 184
-  expect_snapshot(bin_space(occdf = tetrapods), error = TRUE)
+
+  # wrong columns
+  expect_snapshot(
+    bin_space(occdf = tetrapods, lng = "long", lat = "latit"),
+    error = TRUE
+  )
+
+  # spacing and sub_grid give the same resolution
+  expect_snapshot(
+    bin_space(occdf = tetrapods, spacing = 1000, sub_grid = 1000),
+    error = TRUE
+  )
+  # lat must be a numeric value between -90 and 90
+  occdf$lat[1] <- 94
+  expect_snapshot(bin_space(occdf = occdf), error = TRUE)
+  occdf$lat[1] <- "94"
+  expect_snapshot(bin_space(occdf = occdf), error = TRUE)
+
+  # lng must be a numeric value between -180 and 180
+  occdf <- tetrapods
+  occdf$lng[1] <- 184
+  expect_snapshot(bin_space(occdf = occdf), error = TRUE)
+  occdf$lng[1] <- "184"
+  expect_snapshot(bin_space(occdf = occdf), error = TRUE)
 })
