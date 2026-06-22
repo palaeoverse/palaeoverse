12bad26e5ad9b5847c63adb7972427f5195e9c89
etiennebacher <etienne.bacher@protonmail.com>
2026-06-22 11:43:28 +0100

    add helper to gracefully handle connectivity issues

--- vignettes/check-rds-phanerozoic-reefs.R
+++ vignettes/check-rds-phanerozoic-reefs.R
@@ -7,16 +7,17 @@
 
 library(here)
 devtools::load_all()
+source(here("vignettes/check-rds-helpers.R"))
 
 data(reefs)
 data(interval_key)
 
 ### All modifications applied to `reefs` in the vignette
 reefs <- look_up(
-     occdf = reefs,
-     early_interval = "interval",
-     late_interval = "interval",
-     int_key = interval_key
+  occdf = reefs,
+  early_interval = "interval",
+  late_interval = "interval",
+  int_key = interval_key
 )
 
 reefs <- subset(reefs, interval_max_ma <= 541)
@@ -26,45 +27,45 @@ colnames(reefs)[which(colnames(reefs) == "interval_min_ma")] <- "min_ma"
 reefs <- bin_time(occdf = reefs, bins = bins, method = "all")
 
 ### Original call (as displayed in the vignette)
-reefs_new <- palaeorotate(
-     occdf = reefs,
-     age = "bin_midpoint",
-     method = "point",
-     model = "PALEOMAP"
-)
+reefs_new <- fetch_or_skip(palaeorotate(
+  occdf = reefs,
+  age = "bin_midpoint",
+  method = "point",
+  model = "PALEOMAP"
+))
 
 ### Results that we previously stored
 reefs_stored <- readRDS(here("vignettes/_data/palaeorotate_point_paleomap.rds"))
 
 if (!identical(reefs_new, reefs_stored)) {
-     stop(
-          'Results for the following call have changed compared to stored results:
+  stop(
+    'Results for the following call have changed compared to stored results:
 
 palaeorotate(occdf = reefs, age = "bin_midpoint", method = "point", model = "PALEOMAP")'
-     )
+  )
 }
 
 
 ### Original call (as displayed in the vignette)
 models <- c("GOLONKA", "PALEOMAP", "MERDITH2021")
-reefs_new_2 <- palaeorotate(
-     # Difference with vignette:
-     # in the vignette, we use `occdf = reefs` because the results of the
-     # previous palaeorotate() calls are assigned to `reefs` while they are
-     # assigned to `reefs_new` here.
-     occdf = reefs_new,
-     age = "bin_midpoint",
-     method = "point",
-     model = models
-)
+reefs_new_2 <- fetch_or_skip(palaeorotate(
+  # Difference with vignette:
+  # in the vignette, we use `occdf = reefs` because the results of the
+  # previous palaeorotate() calls are assigned to `reefs` while they are
+  # assigned to `reefs_new` here.
+  occdf = reefs_new,
+  age = "bin_midpoint",
+  method = "point",
+  model = models
+))
 
 ### Results that we previously stored
 reefs_stored_2 <- readRDS("vignettes/_data/palaeorotate_point_multi_models.rds")
 
 if (!identical(reefs_new_2, reefs_stored_2)) {
-     stop(
-          'Results for the following call have changed compared to stored results:
+  stop(
+    'Results for the following call have changed compared to stored results:
 
 palaeorotate(occdf = reefs, age = "bin_midpoint", method = "point", model = models)'
-     )
+  )
 }
--- vignettes/check-rds-tetrapod-biodiversity.R
+++ vignettes/check-rds-tetrapod-biodiversity.R
@@ -7,26 +7,27 @@
 
 library(here)
 devtools::load_all()
+source(here("vignettes/check-rds-helpers.R"))
 
 ### All modifications applied to `tetrapods` in the vignette
 data(tetrapods)
 bins <- time_bins(
-     interval = c("Carboniferous", "Permian"),
-     rank = "stage",
-     scale = "GTS2012",
-     plot = FALSE
+  interval = c("Carboniferous", "Permian"),
+  rank = "stage",
+  scale = "GTS2012",
+  plot = FALSE
 )
 tetrapods <- look_up(tetrapods, int_key = interval_key)
 
 tetrapods$interval_max_ma <- ifelse(
-     is.na(tetrapods$interval_max_ma),
-     tetrapods$max_ma,
-     tetrapods$interval_max_ma
+  is.na(tetrapods$interval_max_ma),
+  tetrapods$max_ma,
+  tetrapods$interval_max_ma
 )
 tetrapods$interval_min_ma <- ifelse(
-     is.na(tetrapods$interval_min_ma),
-     tetrapods$min_ma,
-     tetrapods$interval_min_ma
+  is.na(tetrapods$interval_min_ma),
+  tetrapods$min_ma,
+  tetrapods$interval_min_ma
 )
 tetrapods$interval_mid_ma <- (tetrapods$min_ma + tetrapods$max_ma) / 2
 
@@ -36,30 +37,30 @@ colnames(tetrapods)[c(35, 37)] <- c("max_ma", "min_ma")
 cp_tetrapods <- subset(tetrapods, min_ma > min(bins$min_ma))
 
 maj_tetrapods <- bin_time(
-     occdf = cp_tetrapods,
-     bins = bins,
-     method = "majority"
+  occdf = cp_tetrapods,
+  bins = bins,
+  method = "majority"
 )
 
 
 ### Original call (as displayed in the vignette)
-maj_tetrapods_new <- palaeorotate(
-     occdf = maj_tetrapods,
-     age = "bin_midpoint",
-     method = "point",
-     model = "PALEOMAP"
-)
+maj_tetrapods_new <- fetch_or_skip(palaeorotate(
+  occdf = maj_tetrapods,
+  age = "bin_midpoint",
+  method = "point",
+  model = "PALEOMAP"
+))
 
 
 ### Results that we previously stored
 maj_tetrapods_stored <- readRDS(
-     here("vignettes/_data/palaeorotate_point_paleomap_tetrapods.rds")
+  here("vignettes/_data/palaeorotate_point_paleomap_tetrapods.rds")
 )
 
 if (!identical(maj_tetrapods_new, maj_tetrapods_stored)) {
-     stop(
-          'Results for the following call have changed compared to stored results:
+  stop(
+    'Results for the following call have changed compared to stored results:
 
 palaeorotate(occdf = maj_tetrapods, age = "bin_midpoint", method = "point", model = "PALEOMAP")'
-     )
+  )
 }
