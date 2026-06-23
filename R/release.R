683f195a2262956be315d56a3937869b4a7f7a26
etiennebacher <etienne.bacher@protonmail.com>
2026-06-23 13:56:16 +0100

    update message in release.R

--- R/release.R
+++ R/release.R
@@ -1,6 +1,6 @@
 # jarl-ignore unused_function: this is used in usethis::use_release_issue
 release_questions <- function() {
   c(
-    "Did you run `vignettes/check-rds-*.R` to ensure that RDS files used in vignettes are up-to-date?"
+    "Did you run `vignettes/update-rds-files.R` to ensure that RDS files used in vignettes are up-to-date?"
   )
 }
