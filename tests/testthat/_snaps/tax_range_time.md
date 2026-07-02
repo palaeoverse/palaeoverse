972a975ebf3d0e9259a9fd895fafb80e6dfec00b
etiennebacher <etienne.bacher@protonmail.com>
2026-07-02 09:55:45 +0100

    init

--- tests/testthat/_snaps/tax_range_time.md
+++ tests/testthat/_snaps/tax_range_time.md
@@ -1,4 +1,20 @@
-# tax_range_time() works
+# basic behavior works
+
+    Code
+      tax_range_time(occdf = data.frame())
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf = NULL)
+    Condition
+      Error in `tax_range_time()`:
+      ! `occdf` should be a dataframe
+
+---
 
     Code
       tax_range_time(occdf = NA)
@@ -9,47 +25,15 @@
 ---
 
     Code
-      tax_range_time(occdf = occdf, max_ma = "test")
-    Condition
-      Error in `[.data.frame`:
-      ! undefined columns selected
-
----
-
-    Code
-      tax_range_time(occdf = occdf, min_ma = "test")
-    Condition
-      Error in `[.data.frame`:
-      ! undefined columns selected
-
----
-
-    Code
-      tax_range_time(occdf = occdf, by = "test")
+      tax_range_time(occdf = "a")
     Condition
       Error in `tax_range_time()`:
-      ! `by` must be either "FAD", "LAD", or "name"
+      ! `occdf` should be a dataframe
 
----
-
-    Code
-      tax_range_time(occdf = occdf, group = "test")
-    Condition
-      Error in `tax_range_time()`:
-      ! `group` is not a named column in `occdf`
-
----
+# argument 'name' works
 
     Code
-      tax_range_time(occdf = occdf, plot = "test")
-    Condition
-      Error in `tax_range_time()`:
-      ! `plot` should be logical (TRUE/FALSE)
-
----
-
-    Code
-      tax_range_time(occdf = occdf, name = "test")
+      tax_range_time(occdf, name = c("Species", "max_ma"))
     Condition
       Error in `tax_range_time()`:
       ! Either `name`, `min_ma`, or `max_ma`, is not a named column in
@@ -58,7 +42,242 @@
 ---
 
     Code
-      tax_range_time(occdf = occdf, plot_args = "test")
+      tax_range_time(occdf, name = "nonexistent")
+    Condition
+      Error in `tax_range_time()`:
+      ! Either `name`, `min_ma`, or `max_ma`, is not a named column in
+               `occdf`
+
+---
+
+    Code
+      tax_range_time(occdf, name = 1)
+    Condition
+      Error in `tax_range_time()`:
+      ! Either `name`, `min_ma`, or `max_ma`, is not a named column in
+               `occdf`
+
+---
+
+    Code
+      tax_range_time(occdf, name = NA)
+    Condition
+      Error in `tax_range_time()`:
+      ! Either `name`, `min_ma`, or `max_ma`, is not a named column in
+               `occdf`
+
+---
+
+    Code
+      tax_range_time(occdf, name = NULL)
+    Condition
+      Error in `xtfrm.data.frame()`:
+      ! cannot xtfrm data frames
+
+---
+
+    Code
+      tax_range_time(nadf, name = "species")
+    Condition
+      Error in `tax_range_time()`:
+      ! The `name` column contains NA values
+
+# argument 'max_ma' works
+
+    Code
+      tax_range_time(occdf, max_ma = c("Species", "max_ma"))
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf, max_ma = "nonexistent")
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf, max_ma = 1)
+    Condition
+      Error in `tax_range_time()`:
+      ! `max_ma` and `min_ma` must be of class numeric.
+
+---
+
+    Code
+      tax_range_time(occdf, max_ma = NA)
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf, max_ma = NULL)
+    Condition
+      Error in `tax_range_time()`:
+      ! `max_ma` and `min_ma` must be of class numeric.
+
+---
+
+    Code
+      tax_range_time(chardf)
+    Condition
+      Error in `tax_range_time()`:
+      ! `max_ma` and `min_ma` must be of class numeric.
+
+---
+
+    Code
+      tax_range_time(nadf)
+    Condition
+      Error in `tax_range_time()`:
+      ! `min_ma` and/or `max_ma` columns contain NA values
+
+# argument 'min_ma' works
+
+    Code
+      tax_range_time(occdf, min_ma = c("Species", "min_ma"))
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf, min_ma = "nonexistent")
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf, min_ma = 1)
+    Condition
+      Error in `tax_range_time()`:
+      ! `max_ma` and `min_ma` must be of class numeric.
+
+---
+
+    Code
+      tax_range_time(occdf, min_ma = NA)
+    Condition
+      Error in `[.data.frame`:
+      ! undefined columns selected
+
+---
+
+    Code
+      tax_range_time(occdf, min_ma = NULL)
+    Condition
+      Error in `tax_range_time()`:
+      ! `max_ma` and `min_ma` must be of class numeric.
+
+---
+
+    Code
+      tax_range_time(chardf)
+    Condition
+      Error in `tax_range_time()`:
+      ! `max_ma` and `min_ma` must be of class numeric.
+
+---
+
+    Code
+      tax_range_time(nadf)
+    Condition
+      Error in `tax_range_time()`:
+      ! `min_ma` and/or `max_ma` columns contain NA values
+
+# argument 'group' works
+
+    Code
+      tax_range_time(occdf, group = c("genus", "min_ma"))
+    Condition
+      Error in `tax_range_time()`:
+      ! `group` length is >1, only a single grouping variable is accepted.
+
+---
+
+    Code
+      tax_range_time(occdf, group = "nonexistent")
+    Condition
+      Error in `tax_range_time()`:
+      ! `group` is not a named column in `occdf`
+
+---
+
+    Code
+      tax_range_time(occdf, group = 1)
+    Condition
+      Error in `tax_range_time()`:
+      ! `group` is not a named column in `occdf`
+
+---
+
+    Code
+      tax_range_time(occdf, group = NA)
+    Condition
+      Error in `tax_range_time()`:
+      ! `group` is not a named column in `occdf`
+
+# argument 'by' works
+
+    Code
+      tax_range_time(occdf, by = c("genus", "min_ma"))
+    Condition
+      Error in `tax_range_time()`:
+      ! `by` must be of length 1.
+
+---
+
+    Code
+      tax_range_time(occdf, by = "nonexistent")
+    Condition
+      Error in `tax_range_time()`:
+      ! `by` must be either "FAD", "LAD", or "name"
+
+---
+
+    Code
+      tax_range_time(occdf, by = 1)
+    Condition
+      Error in `tax_range_time()`:
+      ! `by` must be either "FAD", "LAD", or "name"
+
+---
+
+    Code
+      tax_range_time(occdf, by = NA)
+    Condition
+      Error in `tax_range_time()`:
+      ! `by` must be either "FAD", "LAD", or "name"
+
+# argument 'plot' works
+
+    Code
+      tax_range_time(occdf, plot = "test")
+    Condition
+      Error in `tax_range_time()`:
+      ! `plot` should be logical (TRUE/FALSE)
+
+---
+
+    Code
+      tax_range_time(occdf, plot = NA)
+    Condition
+      Error in `tax_range_time()`:
+      ! `plot` should be logical (TRUE/FALSE)
+
+# argument 'plot_args' works
+
+    Code
+      tax_range_time(occdf, plot_args = "test")
     Condition
       Error in `tax_range_time()`:
       ! `plot_args` must be either NULL, or a list
@@ -66,16 +285,8 @@
 ---
 
     Code
-      tax_range_time(occdf = occdf)
+      tax_range_time(occdf, plot_args = NA)
     Condition
       Error in `tax_range_time()`:
-      ! The `name` column contains NA values
-
----
-
-    Code
-      tax_range_time(occdf = occdf)
-    Condition
-      Error in `tax_range_time()`:
-      ! `max_ma` and `min_ma` must be of class numeric.
+      ! `plot_args` must be either NULL, or a list
 
--- tests/testthat/test-tax_range_time.R
+++ tests/testthat/test-tax_range_time.R
@@ -1,51 +1,331 @@
-test_that("tax_range_time() works", {
-  occdf <- tetrapods
-
-  occdf <- subset(occdf, !is.na(genus))
-
-  unique_taxa <- length(unique(occdf$genus))
-
-  # Expect true
-  expect_true(is.data.frame(tax_range_time(occdf = occdf)))
-  expect_true(is.data.frame(tax_range_time(occdf = occdf, by = "LAD")))
-  expect_true(is.data.frame(tax_range_time(occdf = occdf, by = "name")))
-  expect_true(is.data.frame(tax_range_time(
-    occdf = occdf,
-    group = "class",
-    plot = TRUE
-  )))
-
-  # Expect equal
-  expect_equal(
-    nrow(tax_range_time(occdf = occdf, plot = TRUE)),
-    unique_taxa
-  )
-  expect_equal(
-    nrow(tax_range_time(occdf = occdf, plot = FALSE)),
-    unique_taxa
+test_that("basic behavior works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
   )
 
-  # Expect error
+  expect_equal(
+    tax_range_time(occdf),
+    data.frame(
+      taxon = c("C", "B", "A"),
+      taxon_id = 1:3,
+      max_ma = c(3, 6, 10),
+      min_ma = c(2, 4, 7),
+      range_myr = c(1, 2, 3),
+      n_occ = c(1L, 2L, 2L)
+    )
+  )
+
+  # input checks
+  expect_snapshot(tax_range_time(occdf = data.frame()), error = TRUE)
+  expect_snapshot(tax_range_time(occdf = NULL), error = TRUE)
   expect_snapshot(tax_range_time(occdf = NA), error = TRUE)
-  expect_snapshot(tax_range_time(occdf = occdf, max_ma = "test"), error = TRUE)
-  expect_snapshot(tax_range_time(occdf = occdf, min_ma = "test"), error = TRUE)
-  expect_snapshot(tax_range_time(occdf = occdf, by = "test"), error = TRUE)
-  expect_snapshot(tax_range_time(occdf = occdf, group = "test"), error = TRUE)
-  expect_snapshot(tax_range_time(occdf = occdf, plot = "test"), error = TRUE)
-  expect_snapshot(tax_range_time(occdf = occdf, name = "test"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf = "a"), error = TRUE)
+})
+
+test_that("argument 'name' works", {
+  occdf <- data.frame(
+    species = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  expect_equal(
+    tax_range_time(occdf, name = "species"),
+    data.frame(
+      taxon = c("C", "B", "A"),
+      taxon_id = 1:3,
+      max_ma = c(3, 6, 10),
+      min_ma = c(2, 4, 7),
+      range_myr = c(1, 2, 3),
+      n_occ = c(1L, 2L, 2L)
+    )
+  )
+
+  # input checks
   expect_snapshot(
-    tax_range_time(occdf = occdf, plot_args = "test"),
+    tax_range_time(occdf, name = c("Species", "max_ma")),
     error = TRUE
   )
-  occdf$genus[1] <- NA
-  expect_snapshot(tax_range_time(occdf = occdf), error = TRUE)
-  occdf$max_ma[1] <- "test"
-  expect_snapshot(tax_range_time(occdf = occdf), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, name = "nonexistent"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, name = 1), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, name = NA), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, name = NULL), error = TRUE)
+
+  # the "name" column must not contain NA values
+  nadf <- occdf
+  nadf$species[1] <- NA
+  expect_snapshot(
+    tax_range_time(nadf, name = "species"),
+    error = TRUE
+  )
 })
-test_that("tax_range_time() resets row names", {
-  # Remove NAs
-  tetrapods <- subset(tetrapods, !is.na(order) & order != "NO_ORDER_SPECIFIED")
-  # Temporal range
-  result <- tax_range_time(occdf = tetrapods, name = "order")
-  expect_equal(row.names(result), as.character(seq_len(nrow(result))))
+
+test_that("argument 'max_ma' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    p_max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  expect_equal(
+    tax_range_time(occdf, max_ma = "p_max_ma"),
+    data.frame(
+      taxon = c("C", "B", "A"),
+      taxon_id = 1:3,
+      max_ma = c(3, 6, 10),
+      min_ma = c(2, 4, 7),
+      range_myr = c(1, 2, 3),
+      n_occ = c(1L, 2L, 2L)
+    )
+  )
+
+  # input checks
+  expect_snapshot(
+    tax_range_time(occdf, max_ma = c("Species", "max_ma")),
+    error = TRUE
+  )
+  expect_snapshot(tax_range_time(occdf, max_ma = "nonexistent"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, max_ma = 1), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, max_ma = NA), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, max_ma = NULL), error = TRUE)
+
+  # the "max_ma" column must be numeric
+  chardf <- data.frame(genus = "a", max_ma = "10", min_ma = 5)
+  expect_snapshot(
+    tax_range_time(chardf),
+    error = TRUE
+  )
+  # the "max_ma" column must not contain NA values
+  nadf <- data.frame(genus = c("a", "b"), max_ma = c(10, NA), min_ma = 5)
+  expect_snapshot(
+    tax_range_time(nadf),
+    error = TRUE
+  )
+})
+
+test_that("argument 'min_ma' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    p_min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  expect_equal(
+    tax_range_time(occdf, min_ma = "p_min_ma"),
+    data.frame(
+      taxon = c("C", "B", "A"),
+      taxon_id = 1:3,
+      max_ma = c(3, 6, 10),
+      min_ma = c(2, 4, 7),
+      range_myr = c(1, 2, 3),
+      n_occ = c(1L, 2L, 2L)
+    )
+  )
+
+  # input checks
+  expect_snapshot(
+    tax_range_time(occdf, min_ma = c("Species", "min_ma")),
+    error = TRUE
+  )
+  expect_snapshot(tax_range_time(occdf, min_ma = "nonexistent"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, min_ma = 1), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, min_ma = NA), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, min_ma = NULL), error = TRUE)
+
+  # the "min_ma" column must be numeric
+  chardf <- data.frame(genus = "a", max_ma = 10, min_ma = "5")
+  expect_snapshot(
+    tax_range_time(chardf),
+    error = TRUE
+  )
+  # the "min_ma" column must not contain NA values
+  nadf <- data.frame(genus = c("a", "b"), max_ma = 10, min_ma = c(5, NA))
+  expect_snapshot(
+    tax_range_time(nadf),
+    error = TRUE
+  )
+})
+
+test_that("argument 'group' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2),
+    family = c("F1", "F1", "F1", "F2", "F2")
+  )
+
+  expect_equal(
+    tax_range_time(occdf, group = "family"),
+    data.frame(
+      taxon = c("B", "A", "C", "B"),
+      taxon_id = 1:4,
+      max_ma = c(6, 10, 3, 5),
+      min_ma = c(5, 7, 2, 4),
+      range_myr = c(1, 3, 1, 1),
+      n_occ = c(1L, 2L, 1L, 1L),
+      family = c("F1", "F1", "F2", "F2")
+    )
+  )
+
+  # input checks
+  expect_snapshot(
+    tax_range_time(occdf, group = c("genus", "min_ma")),
+    error = TRUE
+  )
+  expect_snapshot(tax_range_time(occdf, group = "nonexistent"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, group = 1), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, group = NA), error = TRUE)
+})
+
+test_that("argument 'by' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  expect_equal(
+    tax_range_time(occdf),
+    data.frame(
+      taxon = c("C", "B", "A"),
+      taxon_id = 1:3,
+      max_ma = c(3, 6, 10),
+      min_ma = c(2, 4, 7),
+      range_myr = c(1, 2, 3),
+      n_occ = c(1L, 2L, 2L)
+    )
+  )
+  expect_equal(
+    tax_range_time(occdf, by = "LAD"),
+    data.frame(
+      taxon = c("C", "B", "A"),
+      taxon_id = 1:3,
+      max_ma = c(3, 6, 10),
+      min_ma = c(2, 4, 7),
+      range_myr = c(1, 2, 3),
+      n_occ = c(1L, 2L, 2L)
+    )
+  )
+  expect_equal(
+    tax_range_time(occdf, by = "name"),
+    data.frame(
+      taxon = c("A", "B", "C"),
+      taxon_id = 1:3,
+      max_ma = c(10, 6, 3),
+      min_ma = c(7, 4, 2),
+      range_myr = c(3, 2, 1),
+      n_occ = c(2L, 2L, 1L)
+    )
+  )
+
+  # input checks
+  expect_snapshot(
+    tax_range_time(occdf, by = c("genus", "min_ma")),
+    error = TRUE
+  )
+  expect_snapshot(tax_range_time(occdf, by = "nonexistent"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, by = 1), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, by = NA), error = TRUE)
+})
+
+test_that("argument 'plot' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  # The returned data.frame is identical whether or not a plot is produced
+  expect_equal(
+    tax_range_time(occdf, plot = TRUE),
+    tax_range_time(occdf, plot = FALSE)
+  )
+
+  expect_doppelganger("tax_range_time() works", function() {
+    tax_range_time(occdf)
+  })
+  expect_doppelganger("tax_range_time() works with LAD sorting", function() {
+    tax_range_time(occdf, by = "LAD")
+  })
+  expect_doppelganger("tax_range_time() works with name sorting", function() {
+    tax_range_time(occdf, by = "name")
+  })
+
+  # input checks
+  expect_snapshot(tax_range_time(occdf, plot = "test"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, plot = NA), error = TRUE)
+})
+
+test_that("argument 'plot_args' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  # Passing plot_args does not change the returned data.frame
+  expect_equal(
+    tax_range_time(occdf, plot = TRUE, plot_args = list(ylab = "Taxa")),
+    tax_range_time(occdf, plot = FALSE)
+  )
+
+  expect_doppelganger("tax_range_time() works with plot args", function() {
+    tax_range_time(occdf, plot_args = list(ylab = "Taxa"))
+  })
+
+  # input checks
+  expect_snapshot(tax_range_time(occdf, plot_args = "test"), error = TRUE)
+  expect_snapshot(tax_range_time(occdf, plot_args = NA), error = TRUE)
+})
+
+test_that("argument 'intervals' works", {
+  occdf <- data.frame(
+    genus = c("A", "A", "B", "B", "C"),
+    max_ma = c(10, 8, 6, 5, 3),
+    min_ma = c(9, 7, 5, 4, 2)
+  )
+
+  # Passing intervals does not change the returned data.frame
+  expect_equal(
+    tax_range_time(occdf, plot = TRUE, intervals = "epochs"),
+    tax_range_time(occdf, plot = FALSE)
+  )
+
+  # input checks
+
+  # TODO: currently these do not work because they produce the plot and then throw the error.
+  # The fact that they still create a plot doesn't play nicely with expect_snapshot().
+  # The validation of "intervals" should come earlier in the function so that the plot
+  # isn't created.
+
+  # expect_snapshot(
+  #   tax_range_time(occdf, plot = TRUE, intervals = c("genus", "min_ma")),
+  #   error = TRUE
+  # )
+  # expect_snapshot(
+  #   tax_range_time(occdf, plot = TRUE, intervals = "nonexistent"),
+  #   error = TRUE
+  # )
+  # expect_snapshot(
+  #   tax_range_time(occdf, plot = TRUE, intervals = 1),
+  #   error = TRUE
+  # )
+  # expect_snapshot(
+  #   tax_range_time(occdf, plot = TRUE, intervals = NA),
+  #   error = TRUE
+  # )
+
+  # TODO: should these error if plot = FALSE since intervals would be irrelevant in this case?
+  # expect_snapshot(
+  #   tax_range_time(occdf, intervals = c("genus", "min_ma")),
+  #   error = TRUE
+  # )
+  # expect_snapshot(
+  #   tax_range_time(occdf, intervals = "nonexistent"),
+  #   error = TRUE
+  # )
+  # expect_snapshot(tax_range_time(occdf, intervals = 1), error = TRUE)
+  # expect_snapshot(tax_range_time(occdf, intervals = NA), error = TRUE)
 })
