3a81cc21b22b4e517ad5a88e5233401c836d3c5c
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2026-07-14 09:43:05 +0100

    Skip code coverage for `release()` (#260)

--- R/release.R
+++ R/release.R
@@ -1,6 +1,8 @@
+# nocov start
 # jarl-ignore unused_function: this is used in usethis::use_release_issue
 release_questions <- function() {
   c(
     "Did you run `vignettes/update-rds-files.R` to ensure that RDS files used in vignettes are up-to-date?"
   )
 }
+# nocov end
