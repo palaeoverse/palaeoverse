a5c1464ce26ef1988a7d1c73870305b1d1349143
etiennebacher <etienne.bacher@protonmail.com>
2026-07-22 13:31:18 +0100

    typo [skip ci]

--- tests/testthat/test-tax_range_strat.R
+++ tests/testthat/test-tax_range_strat.R
@@ -56,7 +56,7 @@ test_that("argument 'name' works", {
   )
 
   # input checks
-  # Those give a warning instead of a error on R < 4.3
+  # Those give a warning instead of an error on R < 4.3
   skip_if(getRversion() < "4.3")
   expect_snapshot(tax_range_strat(occdf, name = "test"), error = TRUE)
   expect_snapshot(tax_range_strat(occdf, name = character(0)), error = TRUE)
