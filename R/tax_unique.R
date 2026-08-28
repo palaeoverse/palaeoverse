3737c6f3ce0f0c209ea82b1a7441e9ef55cd231c
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2026-08-28 16:46:46 +0200

    fix underscores in regex

--- R/tax_unique.R
+++ R/tax_unique.R
@@ -226,7 +226,7 @@ tax_unique <- function(
   }
 
   if (
-    !is.null(binomial) && any(grepl("[^[:alnum:][:space:]]", occdf[[binomial]]))
+    !is.null(binomial) && any(grepl("[^[:alnum:][:space:]_]", occdf[[binomial]]))
   ) {
     stop(
       "`binomial` column should not contain punctuation except spaces or
@@ -234,7 +234,7 @@ tax_unique <- function(
     )
   }
 
-  if (!is.null(name) && any(grepl("[^[:alnum:][:space:]]", occdf[[name]]))) {
+  if (!is.null(name) && any(grepl("[^[:alnum:][:space:]_]", occdf[[name]]))) {
     stop(
       "`name` column should not contain punctuation except spaces or
          underscores"
