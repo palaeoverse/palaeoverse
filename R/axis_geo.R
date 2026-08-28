a4bd9d5a33d3e85e6b2332d117ba8c1bebb3426b
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2026-08-28 15:47:04 +0200

    fix formatting

--- R/axis_geo.R
+++ R/axis_geo.R
@@ -825,12 +825,11 @@ axis_geo <- function(
     if (phylo) {
       tick_at <- (tick_at - phylo_alpha) / phylo_beta
     }
-    if (is.null(round)){
+    if (is.null(round)) {
       tick_labels <- as.character(tick_at) # removes trailing zeros
     } else {
       tick_labels <- round(tick_at, digits = round)
     }
-
   }
   if (phylo) {
     if (is.null(tick_at)) {
