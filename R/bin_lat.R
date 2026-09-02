2c495c5cfc7bc9adc74ff67958c2560304a839a2
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2026-09-02 13:42:39 +0100

    Check that args are named in `bin_lat()` (#303)

--- R/bin_lat.R
+++ R/bin_lat.R
@@ -35,6 +35,8 @@
 #' occdf <- bin_lat(occdf = occdf, bins = bins, lat = "lat")
 #'
 bin_lat <- function(occdf, bins, lat = "lat", boundary = FALSE) {
+  ensure_args_are_named()
+
   check_data_frame(occdf)
   check_data_frame(bins)
   rlang::check_bool(boundary)
