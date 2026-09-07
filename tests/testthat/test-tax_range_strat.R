8cec1fe17ffebee644d81821994e3f7d726365f7
etiennebacher <etienne.bacher@protonmail.com>
2026-09-07 11:58:50 +0100

    same

--- tests/testthat/test-tax_range_strat.R
+++ tests/testthat/test-tax_range_strat.R
@@ -86,10 +86,7 @@ test_that("argument 'name' works", {
   # Those give a warning instead of an error on R < 4.3
   skip_if(getRversion() < "4.3")
   expect_snapshot(tax_range_strat(occdf, name = "test"), error = TRUE)
-  expect_snapshot(
-    tax_range_strat(occdf, name = character(0)),
-    error = TRUE
-  )
+  expect_snapshot(tax_range_strat(occdf, name = character(0)), error = TRUE)
   expect_snapshot(tax_range_strat(occdf, name = NA), error = TRUE)
   expect_snapshot(tax_range_strat(occdf, name = 1), error = TRUE)
   nadf <- occdf
