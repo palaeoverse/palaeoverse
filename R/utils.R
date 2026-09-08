6f4efd4d033ae7114fe8e5abd1f673afaec1ef20
etiennebacher <etienne.bacher@protonmail.com>
2026-09-08 12:40:08 +0100

    fix

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,20 @@
 #' @noRd
 ensure_args_are_named <- function(exceptions = NULL) {
   args_in_call_from_user <- rlang::call_args_names(rlang::caller_call())
+
+  # When the function that is being checked is used internally, the call may contain
+  # `...` (e.g. when `tax_unique()` is called from `group_apply()`). We remove `...`
+  # from the count of unnamed arguments.
+  args_values_in_call_from_user <- rlang::call_args(rlang::caller_call())
+  dots_explicitly_passed <- vapply(
+    args_values_in_call_from_user,
+    function(x) {
+      identical(rlang::expr_text(x), "...")
+    },
+    FUN.VALUE = logical(1)
+  )
+  args_in_call_from_user <- args_in_call_from_user[!dots_explicitly_passed]
+
   unnamed_exceptions <- setdiff(exceptions, args_in_call_from_user)
   unnamed_args <- args_in_call_from_user[which(args_in_call_from_user == "")]
   named_args <- args_in_call_from_user[which(
