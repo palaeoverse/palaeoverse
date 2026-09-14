7daba1c2b71b13c241322ba2c6f6b13cbe40c085
etiennebacher <etienne.bacher@protonmail.com>
2026-09-14 11:15:50 +0100

    docs

--- R/as_palaeo.R
+++ R/as_palaeo.R
@@ -8,7 +8,39 @@
 #' @param lat Name of the column that contains the latitude.
 #' @param lon Name of the column that contains the longitude.
 #'
+#' @details
+#' `as_palaeo()` stores information in the data attributes, available via `attributes()`.
+#' It uses explicitly passed values (e.g. `lat = "my_lat"`) and default values if they
+#' exist in the data (e.g. it will store `lat = "lat"` if `"lat"` exists in the dataframe,
+#' even if `lat = "lat"`) isn't explicitly passed.
+#'
+#' This information is used in other `palaeoverse` functions so that users don't need to
+#' pass it everytime. All `palaeoverse` functions use this preference order:
+#'
+#' 1. if a value is explicitly passed in a call, e.g. `bin_lat(lat = "lat", ...)`, then it
+#'    is preferrred;
+#' 2. if the value is not explicitly passed, e.g. `bin_lat(...)` without specifying `lat` then
+#'    the function will first look for the corresponding attribute (in this case, `"lat"`) which
+#'    would have been stored by `as_palaeo()`:
+#'
+#'      a. if this attribute is found, it is used;
+#'      b. otherwise, the default argument value is used.
+#'
 #' @export
+#'
+#' @examples
+#' bins <- lat_bins_degrees(size = 10)
+#'
+#' # We have a dataframe where the latitude values are stored in "my_lat" column
+#' dat <- tetrapods[1:5, ]
+#' names(dat)[names(dat) == "lat"] <- "my_lat"
+#'
+#' # We can store this information in as_palaeo()
+#' dat <- as_palaeo(dat, lat = "my_lat")
+#'
+#' # Other palaeoverse functions that have a "lat" argument will automatically use
+#' # this information (unless another value is explicitly passed in the call)
+#' bin_lat(occdf = dat, bins = bins)
 as_palaeo <- function(x, lat = "lat", lon = "lon") {
   rlang::check_data_frame(x)
 
